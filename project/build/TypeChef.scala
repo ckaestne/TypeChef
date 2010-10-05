6d7e466ea99e9d3be0d2a5a5432a8530957b163d
Christian Kaestner <kaestner@informatik.uni-marburg.de>
2010-10-05 09:17:39 +0200

    rename jcpp->PartialPreprocessor, rename ParserExperiment->ParserFramework

--- project/build/TypeChef.scala
+++ project/build/TypeChef.scala
@@ -2,8 +2,8 @@ import sbt._
 
 class TypeChef(info:ProjectInfo) extends ParentProject(info){
 	lazy val featureexpr = project("FeatureExprLib","FeatureExprLib")
-	lazy val parserexp = project("ParserExperiment","Parser Core",featureexpr)
-	lazy val jcpp = project("jcpp","Partial Preprocessor",featureexpr)
+	lazy val parserexp = project("ParserFramework","Parser Core",featureexpr)
+	lazy val jcpp = project("PartialPreprocessor","Partial Preprocessor",featureexpr)
 	lazy val cparser = project("CParser", "CParser",featureexpr,jcpp,parserexp)
 	lazy val boacasestudy = project("BoaCaseStudy", "BoaCaseStudy",cparser)
 	//val junit = "junit" % "junit" % "4.8"
