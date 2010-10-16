9f51245e64b2be6b663c2a9a6f5f4d37177d71de
Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
2010-10-16 22:09:42 +0200

    Fix build with ecj (force -source 1.5)

--- project/build/TypeChef.scala
+++ project/build/TypeChef.scala
@@ -3,7 +3,14 @@ import sbt._
 class TypeChef(info:ProjectInfo) extends ParentProject(info){
 	lazy val featureexpr = project("FeatureExprLib","FeatureExprLib")
 	lazy val parserexp = project("ParserFramework","Parser Core",featureexpr)
-	lazy val jcpp = project("PartialPreprocessor","Partial Preprocessor",featureexpr)
+	lazy val jcpp = project("PartialPreprocessor", "Partial Preprocessor",
+	  info => new CustomProject(info), featureexpr)
+
+	class CustomProject(info: ProjectInfo) extends DefaultProject(info) {
+	  //-source 1.5 is required for standalone ecj - it defaults to 1.3!
+	  override def javaCompileOptions = super.javaCompileOptions ++ javaCompileOptions("-source", "1.5")
+	}
+
 	lazy val cparser = project("CParser", "CParser",featureexpr,jcpp,parserexp)
 	lazy val boacasestudy = project("BoaCaseStudy", "BoaCaseStudy",cparser)
 	lazy val ctypechecker = project("CTypeChecker", "CTypeChecker",cparser)
