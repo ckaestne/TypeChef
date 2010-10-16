277214f47683b069e78ad9d2fafe5bc90df6bb8a
Paolo 'Blaisorblade' Giarrusso <blaisorblade@yahoo.it>
2010-10-16 22:10:55 +0200

    Whitespace fixes

--- project/build/TypeChef.scala
+++ project/build/TypeChef.scala
@@ -1,8 +1,8 @@
 import sbt._
 
-class TypeChef(info:ProjectInfo) extends ParentProject(info){
-	lazy val featureexpr = project("FeatureExprLib","FeatureExprLib")
-	lazy val parserexp = project("ParserFramework","Parser Core",featureexpr)
+class TypeChef(info: ProjectInfo) extends ParentProject(info) {
+	lazy val featureexpr = project("FeatureExprLib", "FeatureExprLib")
+	lazy val parserexp = project("ParserFramework", "Parser Core", featureexpr)
 	lazy val jcpp = project("PartialPreprocessor", "Partial Preprocessor",
 	  info => new CustomProject(info), featureexpr)
 
@@ -11,9 +11,9 @@ class TypeChef(info:ProjectInfo) extends ParentProject(info){
 	  override def javaCompileOptions = super.javaCompileOptions ++ javaCompileOptions("-source", "1.5")
 	}
 
-	lazy val cparser = project("CParser", "CParser",featureexpr,jcpp,parserexp)
-	lazy val boacasestudy = project("BoaCaseStudy", "BoaCaseStudy",cparser)
-	lazy val ctypechecker = project("CTypeChecker", "CTypeChecker",cparser)
+	lazy val cparser = project("CParser", "CParser", featureexpr, jcpp, parserexp)
+	lazy val boacasestudy = project("BoaCaseStudy", "BoaCaseStudy", cparser)
+	lazy val ctypechecker = project("CTypeChecker", "CTypeChecker", cparser)
 	//val junit = "junit" % "junit" % "4.8"
-	 val junitInterface = "com.novocode" % "junit-interface" % "0.5" % "test->default"
+	val junitInterface = "com.novocode" % "junit-interface" % "0.5" % "test->default"
 }
