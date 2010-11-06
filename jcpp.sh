ea9124ca985bf08bca8c24dcae292ac150f287f3
Paolo Giarrusso <p.giarrusso@gmail.com>
2010-11-06 11:35:12 +0100

    jcpp.sh: fix quoting of arguments (see comments in the source)

--- jcpp.sh
+++ jcpp.sh
@@ -15,6 +15,10 @@ mainClass="PreprocessorFrontend"
 # Brute argument parsing
 # The right thing to do would be to be a gcc replacement, parse its flags and
 # select the ones we care about.
+if [ $# -lt 1 ]; then
+  echo "Not enough arguments!" >&2
+  exit 1
+fi
 inp=$1
 shift
 
@@ -23,13 +27,18 @@ shift
 #time scala -cp BoaCaseStudy/target/scala_2.8.0/classes:FeatureExprLib/lib/org.sat4j.core.jar:FeatureExprLib/target/scala_2.8.0/classes:\
 #  PartialPreprocessor/target/scala_2.8.0/classes:PartialPreprocessor/lib/gnu.getopt.jar \
 #  <(echo -e '#define b ciao\nb')
-bash -c "time scala -cp $basePath/BoaCaseStudy/target/scala_2.8.0/classes:\
+
+# Beware: the embedded for loop requotes the passed argument. That's dark magic,
+# don't ever try to touch it. It simplifies your life as a user of this program
+# though!
+bash -c "time java -cp $basePath/project/boot/scala-2.8.0/lib/scala-library.jar:$basePath/BoaCaseStudy/target/scala_2.8.0/classes:\
 $basePath/FeatureExprLib/lib/org.sat4j.core.jar:\
 $basePath/FeatureExprLib/target/scala_2.8.0/classes:\
 $basePath/PartialPreprocessor/target/scala_2.8.0/classes:\
 $basePath/PartialPreprocessor/lib/gnu.getopt.jar \
-  $mainClass $partialPreprocFlags $@ '$inp' -o '$outPartialPreproc' > '$outDbg' 2> '$outErr'" \
+  $mainClass $partialPreprocFlags \
+  $(for arg in "$@"; do echo -n "\"$arg\" "; done) \
+  '$inp' -o '$outPartialPreproc' > '$outDbg' 2> '$outErr'" \
   2> "$outTime"
 
 mv $macro_stats_path "$outStats" || true
-
