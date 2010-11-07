8329f8859dc66ba95532c1e434c554c19eaf03fb
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2010-11-07 02:34:02 +0100

    boaTestCase.sh now runs on all boa files

--- boaTestCase.sh
+++ boaTestCase.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -vxe
 #!/bin/bash -e
-list="alias boa buffer cgi cgi_header config escape get hash ip log mmap_cache pipe queue read request response 'select' signals sublog util"
-list=alias
+list="alias boa buffer cgi cgi_header config escape get hash ip log mmap_cache pipe queue read request response select signals sublog util"
 flags="-U HAVE_LIBDMALLOC"
 srcPath=boa/src
 
