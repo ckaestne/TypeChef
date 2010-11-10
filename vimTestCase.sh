5dbe042bbfbda36448682d5827244caef9000f70
Paolo Giarrusso <p.giarrusso@gmail.com>
2010-11-10 13:03:00 +0100

    Remove -DNDEBUG, it caused a parsing error while preprocessing, for some reason

--- vimTestCase.sh
+++ vimTestCase.sh
@@ -11,7 +11,7 @@ partialPreprocFlags="$partialPreprocFlags -x FEAT_ -P _H -U FEAT_GUI_MOTIF -U FE
 # variable.
 flags="-I. -I$PWD/$srcPath/proto -DHAVE_CONFIG_H $(pkg-config --cflags gtk+-2.0) -D_FORTIFY_SOURCE=1"
 # From python-config --cflags:
-flags="$flags -I/usr/include/python2.6 -DNDEBUG -D_FORTIFY_SOURCE=2 -D_GNU_SOURCE"
+flags="$flags -I/usr/include/python2.6 -D_FORTIFY_SOURCE=2 -D_GNU_SOURCE"
 # flags="$flags -D FEAT_TINY"
 
 fileList='buffer blowfish charset diff digraph edit eval ex_cmds ex_cmds2 ex_docmd ex_eval ex_getln fileio fold getchar hardcopy hashtab if_cscope if_python if_xcmdsrv main mark memfile memline menu message misc1 misc2 move mbyte normal ops option os_unix popupmnu quickfix regexp screen search sha256 spell syntax tag term ui undo window gui gui_gtk gui_gtk_x11 pty gui_gtk_f gui_beval netbeans version xxd/xxd'
