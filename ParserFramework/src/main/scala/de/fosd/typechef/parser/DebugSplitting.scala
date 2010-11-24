a03fe9763facca8395f34b9ecfa6b4035a800f14
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2010-11-24 15:47:27 +0100

    ParserFramework: enable DebugSplitting

--- ParserFramework/src/main/scala/de/fosd/typechef/parser/DebugSplitting.scala
+++ ParserFramework/src/main/scala/de/fosd/typechef/parser/DebugSplitting.scala
@@ -1,6 +1,6 @@
 package de.fosd.typechef.parser
 
 object DebugSplitting {
-    val DEBUG_SPLITTING = false
+    val DEBUG_SPLITTING = true
     def apply(msg: String) = if (DEBUG_SPLITTING) println(msg)
 }
