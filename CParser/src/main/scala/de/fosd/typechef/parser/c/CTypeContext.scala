84e3ee4356323a8db95c19503cdf6ae07b9cc892
Christian Kaestner <kaestner@informatik.uni-marburg.de>
2010-11-27 21:34:10 +0100

    make type contexts comparable

--- CParser/src/main/scala/de/fosd/typechef/parser/c/CTypeContext.scala
+++ CParser/src/main/scala/de/fosd/typechef/parser/c/CTypeContext.scala
@@ -1,6 +1,9 @@
 package de.fosd.typechef.parser.c
 
-class CTypeContext(val types: Set[String] = Set()) {
+/**
+ * case class to have straightforward hashvalue and equals
+ */
+case class CTypeContext(val types: Set[String] = Set()) {
     def addType(newtype: String) = new CTypeContext(types + newtype)
     def knowsType(typename: String) = types contains typename
 }
