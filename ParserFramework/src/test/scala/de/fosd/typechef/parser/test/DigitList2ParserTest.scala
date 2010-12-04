01d144115d97381e57778dd6632004f32ab515d1
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2010-12-04 07:33:32 +0100

    Rep testcase: abstract code

--- ParserFramework/src/test/scala/de/fosd/typechef/parser/test/DigitList2ParserTest.scala
+++ ParserFramework/src/test/scala/de/fosd/typechef/parser/test/DigitList2ParserTest.scala
@@ -7,6 +7,7 @@ import de.fosd.typechef.featureexpr._
 import org.junit.Test
 
 class DigitList2ParserTest extends TestCase {
+    def newParser = new DigitList2Parser()
 
     val f1 = FeatureExpr.createDefinedExternal("a")
     val f2 = FeatureExpr.createDefinedExternal("b")
@@ -31,7 +32,7 @@ class DigitList2ParserTest extends TestCase {
 
     def testError1() {
         val input = List(t("("), t("3", f1),t(")", f1.not), t(")"))
-        val actual = new DigitList2Parser().parse(input)
+        val actual = newParser.parse(input)
         System.out.println(actual)
         actual match {
             case Success(ast, unparsed) => {
@@ -47,17 +48,17 @@ class DigitList2ParserTest extends TestCase {
         {
             val input = List(t("("), t(")"))
             val expected = DigitList2(List())
-            assertParseResult(expected, new DigitList2Parser().parse(input))
+            assertParseResult(expected, newParser.parse(input))
         }
         {
             val input = List(t("("), t("1"), t(")"))
             val expected = DigitList2(List(o(Lit(1))))
-            assertParseResult(expected, new DigitList2Parser().parse(input))
+            assertParseResult(expected, newParser.parse(input))
         }
         {
             val input = List(t("("), t("1"), t("2"), t(")"))
             val expected = DigitList2(List(o(Lit(1)), o(Lit(2))))
-            assertParseResult(expected, new DigitList2Parser().parse(input))
+            assertParseResult(expected, newParser.parse(input))
         }
     }
 
@@ -65,58 +66,58 @@ class DigitList2ParserTest extends TestCase {
     def testParseOptSimpleList1() {
         val input = List(t("("), t("1", f1), t("2", f1.not), t(")"))
         val expected = DigitList2(List(Opt(f1, Lit(1)),Opt(f1.not, Lit(2))))
-        assertParseResult(expected, new DigitList2Parser().parse(input))
+        assertParseResult(expected, newParser.parse(input))
     }
     def testParseOptSimpleListFirst() {
         val input = List(t("("), t("1", f1), t("1"), t("2"), t(")"))
         val expected = DigitList2(List(Opt(f1, Lit(1)), o(Lit(1)), o(Lit(2))))
-        assertParseResult(expected, new DigitList2Parser().parse(input))
+        assertParseResult(expected, newParser.parse(input))
     }
     def testParseOptSimpleListLast() {
         val input = List(t("("), t("1"), t("2"), t("3", f1), t(")"))
         val expected = DigitList2(List(o(Lit(1)), o(Lit(2)), Opt(f1, Lit(3))))
-        assertParseResult(expected, new DigitList2Parser().parse(input))
+        assertParseResult(expected, newParser.parse(input))
     }
     def testParseOptSimpleListMid() {
         val input = List(t("("), t("1"), t("2", f1), t("3"), t(")"))
         val expected = DigitList2(List(o(Lit(1)), Opt(f1, Lit(2)), o(Lit(3))))
-        assertParseResult(expected, new DigitList2Parser().parse(input))
+        assertParseResult(expected, newParser.parse(input))
     }
     def testParseOptSimpleListCompl1() {
         val input = List(t("("), t("1"), t("2", f1), t("3", f2), t(")"))
         val expected = DigitList2(List(o(Lit(1)), Opt(f1, Lit(2)), Opt(f2, Lit(3))))
-        assertParseResult(expected, new DigitList2Parser().parse(input))
+        assertParseResult(expected, newParser.parse(input))
     }
     def testParseOptSimpleListCompl2() {
         val input = List(t("("), t("1", f2), t("2", f1), t("3", f2), t(")"))
         val expected = DigitList2(List(Opt(f2, Lit(1)), Opt(f1, Lit(2)), Opt(f2, Lit(3))))
-        assertParseResult(expected, new DigitList2Parser().parse(input))
+        assertParseResult(expected, newParser.parse(input))
     }
     def testParseOptSimpleListCompl3() {
         val input = List(t("("), t("1", f2), t("2", f1), t("3", f2.not), t(")"))
         val expected = DigitList2(List(Opt(f2, Lit(1)), Opt(f1, Lit(2)), Opt(f2.not, Lit(3))))
-        assertParseResult(expected, new DigitList2Parser().parse(input))
+        assertParseResult(expected, newParser.parse(input))
     }
     def testParseOptSimpleListCompl4() {
         val input = List(t("("), t("1", f2), t("2", f2.not), t("3", f2.not), t(")"))
         val expected = DigitList2(List(Opt(f2, Lit(1)), Opt(f2.not, Lit(2)), Opt(f2.not, Lit(3))))
-        assertParseResult(expected, new DigitList2Parser().parse(input))
+        assertParseResult(expected, newParser.parse(input))
     }
     def testParseInterleaved1() {
         val input = List(t("("), t("("), t("1"), t("2"), t(")"), t("3"), t(")"))
         val expected = DigitList2(List(o(DigitList2(List(o(Lit(1)), o(Lit(2))))), o(Lit(3))))
-        assertParseResult(expected, new DigitList2Parser().parse(input))
+        assertParseResult(expected, newParser.parse(input))
     }
     def testParseInterleaved2() {
         val input = List(t("("), t("(", f1), t("1"), t("2"), t(")", f1), t("3"), t(")"))
         val expected = Alt(f1, DigitList2(List(o(DigitList2(List(Opt(f1, Lit(1)), Opt(f1, Lit(2))))), o(Lit(3)))), DigitList2(List(o(Lit(1)), o(Lit(2)), o(Lit(3)))))
-        assertParseResult(expected, new DigitList2Parser().parse(input))
+        assertParseResult(expected, newParser.parse(input))
     }
 
     def testNoBacktrace {
         val input = List(t("1"), t("("))
         val expected = Lit(1)
-        var actual = new DigitList2Parser().parse(input)
+        var actual = newParser.parse(input)
         println(actual)
         actual match {
             case Success(ast, unparsed) => fail("expected error, found " + ast + " - " + unparsed)
