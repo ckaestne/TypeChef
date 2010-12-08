ec220d2743dd8f2271070be4bf8df46bc72b19d8
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2010-12-08 13:00:33 +0100

    MultiFeatureContParser: failed attempt at resuming it
    This time I could make more sense of some type errors. It became clear that some
    errors are deeply rooted into this specific formalization of continuation-based
    parsing: for instance, when I resume an old continuation, I need to switch the
    result type from ParseResult[T] to ParseResult[OldT], i.e. to the type of the
    stored continuations, which is incorrect.

--- ParserFramework/src/main/scala/de/fosd/typechef/parser/MultiFeatureContParser.scala
+++ ParserFramework/src/main/scala/de/fosd/typechef/parser/MultiFeatureContParser.scala
@@ -1,5 +1,6 @@
 package de.fosd.typechef.parser
 
+import de.fosd.typechef.featureexpr.FeatureExpr
 import scala.util.continuations._
 import scala.collection.mutable.ArrayStack
 import scala.collection.mutable.Stack
@@ -25,16 +26,17 @@ case object Endif extends PreprocDirective {}
 //  type Elem >: PreprocDirective
 //  override type Input = Reader[Elem]
 //
+//  type CpsParseResult[+T] = ParseResult[T] @cpsParam[ParseResult[T], CpsParser[T]]
 //  /** A base class for parser results. A result is either successful or not (failure may be fatal, i.e., an CpsError, or
 //   * not, i.e., a CpsFailure). On success, provides a result of type `T` which consists of some result (and the rest of
 //   * the input). */
-//  sealed abstract class CpsParseResult[+T] {
+//  sealed abstract class ParseResult[+T] {
 //    /** Functional composition of ParseResults
 //     * 
 //     * @param `f' the function to be lifted over this result
 //     * @return `f' applied to the result of this `CpsParseResult', packaged up as a new `CpsParseResult'
 //     */
-//    def map[U](f: T => U): CpsParseResult[U]
+//    def map[U](f: T => U): ParseResult[U]
 //    
 //    /** Partial functional composition of ParseResults
 //     * 
@@ -45,9 +47,9 @@ case object Endif extends PreprocDirective {}
 //     *         `f' applied to the result of this `CpsParseResult', packaged up as a new `CpsParseResult'.
 //     *         If `f' is not defined, `CpsFailure'.
 //     */
-//    def mapPartial[U](f: PartialFunction[T, U], error: T => String): CpsParseResult[U]   
+//    def mapPartial[U](f: PartialFunction[T, U], error: T => String): ParseResult[U]   
 //    
-//    def flatMapWithNext[U](f: T => Input => CpsParseResult[U]): CpsParseResult[U]     
+//    def flatMapWithNext[U](f: T => CpsParser[U]): ParseResult[U]     
 //
 //    def append[U >: T](a: => CpsParseResult[U]): CpsParseResult[U]
 //    
@@ -69,16 +71,16 @@ case object Endif extends PreprocDirective {}
 //   *  @param result The parser's output 
 //   *  @param next   The parser's remaining input
 //   */
-//  case class CpsSuccess[+T](result: T, override val next: Input) extends CpsParseResult[T] {
+//  case class CpsSuccess[+T](result: T, override val next: Input) extends ParseResult[T] {
 //    def map[U](f: T => U) = CpsSuccess(f(result), next)
-//    def mapPartial[U](f: PartialFunction[T, U], error: T => String): CpsParseResult[U] 
+//    def mapPartial[U](f: PartialFunction[T, U], error: T => String): ParseResult[U] 
 //       = if(f.isDefinedAt(result)) CpsSuccess(f(result), next) 
 //         else CpsFailure(error(result), next)
 //
-//    def flatMapWithNext[U](f: T => CpsParser[U]): CpsParseResult[U] 
+//    def flatMapWithNext[U](f: T => CpsParser[U]): ParseResult[U] 
 //      = f(result)(next) 
 //
-//    def append[U >: T](a: => CpsParseResult[U]): CpsParseResult[U] = this
+//    def append[U >: T](a: => CpsParseResult[U]): ParseResult[U] = this
 //
 //    def get: T = result
 //    
@@ -92,15 +94,15 @@ case object Endif extends PreprocDirective {}
 //
 //  /** A common super-class for unsuccessful parse results
 //   */
-//  sealed abstract class CpsNoSuccess(val msg: String, override val next: Input) extends CpsParseResult[Nothing] { // when we don't care about the difference between CpsFailure and CpsError
+//  sealed abstract class CpsNoSuccess(val msg: String, override val next: Input) extends ParseResult[Nothing] { // when we don't care about the difference between CpsFailure and CpsError
 //    val successful = false
 //    if (!(lastNoSuccess != null && next.pos < lastNoSuccess.next.pos))
 //      lastNoSuccess = this
 //
 //    def map[U](f: Nothing => U) = this
-//    def mapPartial[U](f: PartialFunction[Nothing, U], error: Nothing => String): CpsParseResult[U] = this
+//    def mapPartial[U](f: PartialFunction[Nothing, U], error: Nothing => String): ParseResult[U] = this
 //
-//    def flatMapWithNext[U](f: Nothing => Input => CpsParseResult[U]): CpsParseResult[U] 
+//    def flatMapWithNext[U](f: Nothing => Input => CpsParseResult[U]): ParseResult[U] 
 //      = this
 //
 //    def get: Nothing = error("No result when parsing failed")
@@ -125,7 +127,7 @@ case object Endif extends PreprocDirective {}
 //    /** The toString method of a CpsFailure yields an error message */
 //    override def toString = "["+next.pos+"] failure: "+msg+"\n\n"+next.pos.longString
 //    
-//    def append[U >: Nothing](a: => CpsParseResult[U]): CpsParseResult[U] = { val alt = a; alt match {
+//    def append[U >: Nothing](a: => CpsParseResult[U]): ParseResult[U] = { val alt = a; alt match {
 //      case CpsSuccess(_, _) => alt
 //      case ns: CpsNoSuccess => if (alt.next.pos < next.pos) this else alt
 //    }}
@@ -140,10 +142,10 @@ case object Endif extends PreprocDirective {}
 //  case class CpsError(override val msg: String, override val next: Input) extends CpsNoSuccess(msg, next) {
 //    /** The toString method of an CpsError yields an error message */
 //    override def toString = "["+next.pos+"] error: "+msg+"\n\n"+next.pos.longString
-//    def append[U >: Nothing](a: => CpsParseResult[U]): CpsParseResult[U] = this
+//    def append[U >: Nothing](a: => CpsParseResult[U]): ParseResult[U] = this
 //  }
 //
-//  def resetParser[U](next: CpsParser[U]) = new CpsParser[U] { def apply(in: Input) = reset { next(in) } }
+//  def resetParser[U](next: CpsParser[U]) = (in: Input) => reset { next(in) }
 //  case class ParsingThread[+U](contPars: CpsParser[U], feature: FeatureExpr, captCont: CpsParseResult[U] => CpsParseResult[U])
 //  
 //  case class ParsingThreads[+U](isComplete: Boolean, cont: CpsParser[U], currFeat: FeatureExpr, states: List[ParsingThread[U]])
@@ -156,10 +158,10 @@ case object Endif extends PreprocDirective {}
 //  /* The returned parser fetches the top of the stack, and pushes it again after adding the captured continuation and feature,
 //   * in the list of suspended closures associated with firstNext,
 //   * reinvoke oldNext under reset (maybe the original reset is enough, maybe not).*/
-//  def alternativeParser[U](next: CpsParser[U], newTopFeatOpt: Option[FeatureExpr]) = //: CpsParser[U] =
-//	  ((in: Input) => /*: CpsParseResult[U] @cpsParam[CpsParseResult[U], CpsParser[U]] = {*/
+//  def alternativeParser[U](next: CpsParser[U], newTopFeatOpt: Option[FeatureExpr]) : CpsParser[Any] =
+//	  CpsParser[Any]((in: Input) => /*: CpsParseResult[U] @cpsParam[CpsParseResult[U], CpsParser[U]] = {*/
 //	  shift {
-//	 	  (captCont: CpsParseResult[U] => CpsParseResult[U]) =>
+//	 	  (captCont: CpsParseResult[Any] => CpsParseResult[Any]) =>
 //	 	  val topEntry @ ParsingThreads (isComplete, oldNext, topFeat, currStates) = parsingCtx.pop
 //	 	  val newList = new ParsingThread(next, topFeat, captCont) :: currStates
 //	 	  val negOldFeat = topFeat.not()
@@ -169,11 +171,11 @@ case object Endif extends PreprocDirective {}
 //	 	  })
 //	 	  oldNext
 //	  })
-//  def CpsParser[T](f: Input => CpsParseResult[T]): CpsParser[T] =
+//  def CpsParser[T](f: Input => CpsParseResult[T] @cpsParam[CpsParseResult[T], CpsParser[T]]): CpsParser[T] =
 //	new CpsParser[T]{ def apply(in: Input) = f(in) }
 //  
 //  type CpsParserCont[U] = ControlContext[CpsParseResult[U],CpsParseResult[U],CpsParseResult[Any]]
-//  abstract class CpsParser[+T] extends (Input => CpsParseResult[T] @cpsParam[CpsParseResult[T], CpsParser[Any]]) {
+//  abstract class CpsParser[+T] extends (Input => CpsParseResult[T] @cpsParam[ParseResult[T], CpsParser[T]]) {
 //  //abstract class CpsParser[+T] extends (Input => CpsParseResult[T]) {
 //	private var name: String = ""
 //    def named(n: String): this.type = {name=n; this}
@@ -187,7 +189,10 @@ case object Endif extends PreprocDirective {}
 //    //  (for (a <- this; b <- p) yield new ~(a, b)).named("~")
 //
 //    def baseParserSeq[U](p: Elem => CpsParser[U]): CpsParser[~[T, U]] =
-//    	this.flatMap((resThis: T) => new CpsParser[~[T, U]] {def apply(in: Input) {new ~(resThis, p(in.first)(in)) } })
+//        (for(a <- this; b <- p) yield new ~(a,b)).named("~")
+//    	/*this.flatMap((resThis: T) => CpsParser[~[T, U]] ({
+//    	    (in: Input) => CpsSuccess(new ~(resThis, p(in.first)(in)))
+//    	}))*/
 //      //(for (a <- this; b <- p) yield new ~(a, b)) //.named("~")*/
 //
 //    // XXX: this should be part of a companion Object.
@@ -215,17 +220,18 @@ case object Endif extends PreprocDirective {}
 //    		/* push next somewhere, so as to reinvoke it and to check if we re-share it.*/
 //    		case Elif(feature) => val oldNext = alternativeParser(next, Some(feature)); resetParser(oldNext)
 //    		case Else => val oldNext = alternativeParser(next, None); resetParser(oldNext)
-//    		case Endif =>
-//    		val oldNext2 = alternativeParser(next, None) //resetParser(next)
-//    		//isComplete signals whether this is #if...#endif or #if...#else...#endif!
-//    		val topEntry @ ParsingThreads (isComplete, oldNext, topFeat, currStates) = parsingCtx.pop
-//    		val nextParser = if (isComplete) next else oldNext
-//    		new CpsParser[U] {
-//    			def apply(in: Input) = reset {
-//    				createAltNode (currStates flatMap {
-//    					{ case ParsingThread(next, topFeat, captCont) => if (next == nextParser) List(captCont(nextParser(in))) else Nil }
-//    				})
-//    			}
+//    		case Endif => {
+//    		    val oldNext2 = alternativeParser(next, None) //resetParser(next)
+//    		    //isComplete signals whether this is #if...#endif or #if...#else...#endif!
+//    		    val topEntry @ ParsingThreads (isComplete, oldNext, topFeat, currStates) = parsingCtx.pop
+//    		    val nextParser = if (isComplete) next else oldNext
+//    		    new CpsParser[U] {
+//    		        def apply(in: Input) = reset {
+//    		            createAltNode (currStates flatMap {
+//    		                { case ParsingThread(next, topFeat, captCont) => if (next == nextParser) List(captCont(nextParser(in))) else Nil }
+//    		            })
+//    		        }
+//    		    }
 //    		}
 //        })
 //
