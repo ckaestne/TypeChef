7a9f0ee4812c3eccd368138372613915e0de03d1
Christian Kaestner <kaestner@informatik.uni-marburg.de>
2011-01-04 17:23:37 +0100

    collect global statistics

--- JavaParser/src/test/scala/de/fost/typechef/parser/java15/ParseMobileMedia.scala
+++ JavaParser/src/test/scala/de/fost/typechef/parser/java15/ParseMobileMedia.scala
@@ -1,4 +1,5 @@
 package de.fost.typechef.parser.java15
+
 import de.fosd.typechef.featureexpr.FeatureExpr
 
 import de.fosd.typechef.parser.java15._
@@ -14,13 +15,20 @@ object ParseMobileMedia {
 
     def main(args: Array[String]) {
         checkDir(dir)
+
+        println("Overall statistics:\n" +
+                "    sumParsingTime = " + sumParsingTime +
+                "\n    sumTokens =" + sumTokens +
+                "\n    sumConsumed=" + sumConsumed +
+                "\n    sumBacktracked =" + sumBacktracked +
+                "\n    sumRepeated=" + sumRepeated)
     }
 
     def checkDir(dir: File) {
         if (dir.isFile)
             checkFile(dir)
         else
-            //recurse
+        //recurse
             dir.listFiles.map(checkDir(_))
     }
     def checkFile(file: File) {
@@ -33,25 +41,37 @@ object ParseMobileMedia {
             val tokens = JavaLexer.lexFile(file.getAbsolutePath)
 
             val parserStartTime = System.currentTimeMillis
-            val p=new JavaParser()
+            val p = new JavaParser()
             var ast = p.phrase(p.CompilationUnit)(tokens, FeatureExpr.base)
             val endTime = System.currentTimeMillis
 
             println("Parsing statistics: \n" +
-                       "  Duration lexing: " + (parserStartTime - lexerStartTime) + " ms\n" +
-                       "  Duration parsing: " + (endTime - parserStartTime) + " ms\n" +
-                       "  Tokens: " + tokens.tokens.size + "\n" +
-                       "  Tokens Consumed: " + ProfilingTokenHelper.totalConsumed(tokens) + "\n" +
-                       "  Tokens Backtracked: " + ProfilingTokenHelper.totalBacktracked(tokens) + "\n" +
-                       "  Tokens Repeated: " + ProfilingTokenHelper.totalRepeated(tokens) + "\n"+
-                       "  Repeated Distribution: " + ProfilingTokenHelper.repeatedDistribution(tokens) + "\n")
+                    "  Duration lexing: " + (parserStartTime - lexerStartTime) + " ms\n" +
+                    "  Duration parsing: " + (endTime - parserStartTime) + " ms\n" +
+                    "  Tokens: " + tokens.tokens.size + "\n" +
+                    "  Tokens Consumed: " + ProfilingTokenHelper.totalConsumed(tokens) + "\n" +
+                    "  Tokens Backtracked: " + ProfilingTokenHelper.totalBacktracked(tokens) + "\n" +
+                    "  Tokens Repeated: " + ProfilingTokenHelper.totalRepeated(tokens) + "\n" +
+                    "  Repeated Distribution: " + ProfilingTokenHelper.repeatedDistribution(tokens) + "\n")
+
+            sumParsingTime += (endTime - parserStartTime)
+            sumTokens += tokens.tokens.size
+            sumConsumed += ProfilingTokenHelper.totalConsumed(tokens)
+            sumBacktracked += ProfilingTokenHelper.totalBacktracked(tokens)
+            sumRepeated += ProfilingTokenHelper.totalRepeated(tokens)
 
             ast match {
-                case p.Success(ast, unparsed) => { if (!unparsed.atEnd) println("parser did not reach end of token stream: " + unparsed) }
+                case p.Success(ast, unparsed) => {if (!unparsed.atEnd) println("parser did not reach end of token stream: " + unparsed)}
                 case p.NoSuccess(msg, unparsed, inner) => println(msg + " at " + unparsed + " " + inner)
-                case p.SplittedParseResult(f,a,b)=>println("split")
+                case p.SplittedParseResult(f, a, b) => println("split")
             }
         }
     }
+
+    var sumParsingTime: Long = 0
+    var sumTokens: Int = 0
+    var sumConsumed: Int = 0
+    var sumBacktracked: Int = 0
+    var sumRepeated: Int = 0
 
 }
