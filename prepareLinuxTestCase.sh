9c36e425cbdbe72ae2b5334b88b4ce69d8878717
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-01-15 19:31:38 +0100

    Add patches used to compile the Linux kernel and update prepareLinuxTestCase.sh

--- prepareLinuxTestCase.sh
+++ prepareLinuxTestCase.sh
@@ -4,6 +4,9 @@ version=linux-2.6.33.3
 wget http://www.kernel.org/pub/linux/kernel/v2.6/$version.tar.bz2
 tar xjf $version.tar.bz2
 cd $version
+while read i; do
+  patch -p1 -i $i
+done < linux-2.6.33.3-patches/series
 
 make allnoconfig ARCH=x86
 make prepare ARCH=x86
