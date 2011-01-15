38e6f141d8392591ad9ec1d3d50fa707532e5f66
Paolo Giarrusso <p.giarrusso@gmail.com>
2011-01-16 00:22:49 +0100

    Updates/fixes to flags in vimTestCase.sh (there was a system update, system Python headers were moved)

--- vimTestCase.sh
+++ vimTestCase.sh
@@ -6,9 +6,9 @@ srcPath=vim73/src
 export partialPreprocFlags="-x FEAT_ -P _H -U FEAT_GUI_MOTIF -U FEAT_SUN_WORKSHOP -U FEAT_GUI_MAC -U RISCOS -U FEAT_GUI_PHOTON -U FEAT_GUI_MSWIN -U FEAT_GUI_NEXTAW -U FEAT_MZSCHEME -U FEAT_GUI_GNOME -U FEAT_MOUSE_GPM -U FEAT_SYSMOUSE"
 # Compilation options used for Vim. FEAT_TINY makes sure most features are left
 # variable.
-flags="-I. -I$PWD/$srcPath/proto -DHAVE_CONFIG_H $(pkg-config --cflags gtk+-2.0) -D_FORTIFY_SOURCE=1"
+flags="-I. -I$PWD/$srcPath/proto -DHAVE_CONFIG_H $(pkg-config --cflags gtk+-2.0)" # -D_FORTIFY_SOURCE=1
 # From python-config --cflags:
-flags="$flags -I/usr/include/python2.6 -D_FORTIFY_SOURCE=2 -D_GNU_SOURCE"
+flags="$flags -Isystems/redhat/usr/include/python2.6 -D_FORTIFY_SOURCE=2 -D_GNU_SOURCE"
 # flags="$flags -D FEAT_TINY"
 
 fileList='buffer blowfish charset diff digraph edit eval ex_cmds ex_cmds2 ex_docmd ex_eval ex_getln fileio fold getchar hardcopy hashtab if_cscope if_python if_xcmdsrv main mark memfile memline menu message misc1 misc2 move mbyte normal ops option os_unix popupmnu quickfix regexp screen search sha256 spell syntax tag term ui undo window gui gui_gtk gui_gtk_x11 pty gui_gtk_f gui_beval netbeans version xxd/xxd'
