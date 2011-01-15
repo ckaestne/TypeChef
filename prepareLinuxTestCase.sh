579cd9f7d4fb5765fcdbb0d64fa29b7b3645dbfd
Paolo Giarrusso <p.giarrusso@gmail.com>
2011-01-15 20:04:04 +0100

    prepareLinuxTestCase.sh: fix patching code

--- prepareLinuxTestCase.sh
+++ prepareLinuxTestCase.sh
@@ -1,12 +1,17 @@
 #!/bin/sh -v
 version=linux-2.6.33.3
 
-wget http://www.kernel.org/pub/linux/kernel/v2.6/$version.tar.bz2
-tar xjf $version.tar.bz2
+#tarball=$version.tar.bz2
+#if [ ! -f $tarball ]; then
+#  wget http://www.kernel.org/pub/linux/kernel/v2.6/$tarball
+#fi
+#tar xjf $tarball
+
 cd $version
+patchesPath=../linux-2.6.33.3-patches
 while read i; do
-  patch -p1 -i $i
-done < linux-2.6.33.3-patches/series
+  patch -p1 -i $patchesPath/$i
+done < $patchesPath/series
 
 make allnoconfig ARCH=x86
 make prepare ARCH=x86
