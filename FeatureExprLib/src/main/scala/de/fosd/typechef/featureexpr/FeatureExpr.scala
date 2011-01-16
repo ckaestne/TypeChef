045fe4d989d6b3e099f38091b513f6b757d75e8d
Paolo Giarrusso <p.giarrusso@gmail.com>
2011-01-16 14:31:36 +0100

    Revert "Fix bug in BinaryLogicConnective.mapDefinedExpr"
    This reverts commit c89dcd94b5bc44c32578018aaee3bfe35b3ec5a4, as a crazy attempt
    to fix a performance regression.
    For some reason I do not yet understand, after this revert the code works
    quickly again. I'm positive the bug is real, but it appears to improve
    performance.

--- FeatureExprLib/src/main/scala/de/fosd/typechef/featureexpr/FeatureExpr.scala
+++ FeatureExprLib/src/main/scala/de/fosd/typechef/featureexpr/FeatureExpr.scala
@@ -538,7 +538,7 @@ abstract class BinaryLogicConnective extends FeatureExpr {
         var anyChange = false
         val newClauses = clauses.map(x => {
             val y = x.mapDefinedExpr(f, cache)
-            anyChange |= x != y
+            anyChange |= x == y //XXX: Shouldn't be the opposite?
             y})
         if (anyChange)
             create(newClauses)
