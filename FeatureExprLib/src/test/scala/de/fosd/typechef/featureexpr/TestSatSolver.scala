4c166a23f2314dc8334df97467f1c15edef03883
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2011-01-22 13:51:08 +0100

    TestSatSolver: add test for last equiSatCNF bug

--- FeatureExprLib/src/test/scala/de/fosd/typechef/featureexpr/TestSatSolver.scala
+++ FeatureExprLib/src/test/scala/de/fosd/typechef/featureexpr/TestSatSolver.scala
@@ -10,6 +10,12 @@ class TestSatSolver extends TestCase {
     def a = createDefinedExternal("a")
     def b = createDefinedExternal("b")
     def h = createDefinedExternal("h")
+    //Leave these as def, not val, maybe (???) to test caching more.
+    def c = createDefinedExternal("c")
+
+    def d = createDefinedExternal("d")
+    def e = createDefinedExternal("e")
+    def f = createDefinedExternal("f")
 
     @Test
     def testSATSolver() {
@@ -25,6 +31,14 @@ class TestSatSolver extends TestCase {
         assertEquals(true, dead isContradiction ())
         assertEquals(true, base isTautology ())
         assertEquals(true, createInteger(2).toFeatureExpr.isTautology())
+    }
+
+    @Test def testBrokenSat {
+        val j=(((((d and (c not)) not) or (d and (a)) or (((d not) or c) not)) not) and ((((c or (b not)) and (c or a or (d not) or (c and d) or (a and c))) not) or (((d) not) and ((e or c) not))))
+        val k=d
+
+        assert(((j toCNF) or (k toCNF)).isSatisfiable)
+        assert((j or k).isSatisfiable)
     }
 
     @Test
