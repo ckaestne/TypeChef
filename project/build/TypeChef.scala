d4fd6920a5d73e0160157a659a48699b16c737e8
Paolo Giarrusso <p.giarrusso@gmail.com>
2011-02-17 23:45:54 +0100

    Mov FeatureModelTest to a separate project.

--- project/build/TypeChef.scala
+++ project/build/TypeChef.scala
@@ -14,6 +14,7 @@ class TypeChef(info: ProjectInfo) extends ParentProject(info) with IdeaProject {
     lazy val cparser = project("CParser", "CParser", new DefaultSubProject(_), featureexpr, jcpp, parserexp)
     lazy val boacasestudy = project("BoaCaseStudy", "BoaCaseStudy", new DefaultSubProject(_), cparser, ctypechecker)
     lazy val preprocessorfrontend = project("PreprocessorFrontend", "PreprocessorFrontend", new DefaultSubProject(_), cparser, ctypechecker)
+    lazy val linuxAnalysis = project("LinuxAnalysis", "LinuxAnalysis", new DefaultSubProject(_), featureexpr)
     lazy val ctypechecker = project("CTypeChecker", "CTypeChecker", new DefaultSubProject(_), cparser)
     lazy val javaparser = project("JavaParser", "JavaParser", new DefaultSubProject(_), featureexpr, parserexp)
 
