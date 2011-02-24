5bfca7c6f2acb213c962ff746a60c0da780d626a
Christian Kaestner <kaestner@informatik.uni-marburg.de>
2011-02-24 11:25:46 +0100

    remove webbytest

--- project/build/TypeChef.scala
+++ project/build/TypeChef.scala
@@ -1,6 +1,5 @@
 import sbt._
 import reaktor.scct.ScctProject
-import webbytest.HtmlTestsProject
 
 class TypeChef(info: ProjectInfo) extends ParentProject(info) with IdeaProject {
     val junit = "junit" % "junit" % "4.8.2" % "test->default"
@@ -18,7 +17,7 @@ class TypeChef(info: ProjectInfo) extends ParentProject(info) with IdeaProject {
     lazy val ctypechecker = project("CTypeChecker", "CTypeChecker", new DefaultSubProject(_), cparser)
     lazy val javaparser = project("JavaParser", "JavaParser", new DefaultSubProject(_), featureexpr, parserexp)
 
-    class DefaultSubProject(info: ProjectInfo) extends DefaultProject(info) with ScctProject with IdeaProject with HtmlTestsProject {
+    class DefaultSubProject(info: ProjectInfo) extends DefaultProject(info) with ScctProject with IdeaProject {
         //val junitInterface = "com.novocode" % "junit-interface" % "0.5" % "test->default"
         //val scalacheck = "org.scala-tools.testing" % "scalacheck_2.8.1" % "1.8" % "test->default"
         //		val scalatest = "org.scala-tools.testing" % "scalatest" % "0.9.5" % "test->default"
@@ -28,9 +27,10 @@ class TypeChef(info: ProjectInfo) extends ParentProject(info) with IdeaProject {
             Deprecation, ExplainTypes, Optimize)
     }
 
-    class JavaSubProject(info: ProjectInfo) extends DefaultProject(info) with HtmlTestsProject with ScctProject with IdeaProject {
+    class JavaSubProject(info: ProjectInfo) extends DefaultProject(info) with ScctProject with IdeaProject {
         //-source 1.5 is required for standalone ecj - it defaults to 1.3!
         override def javaCompileOptions = super.javaCompileOptions ++ javaCompileOptions("-source", "1.5", "-Xlint:unchecked")
         val junit = "junit" % "junit" % "4.8.2" % "test->default"
     }
+
 }
