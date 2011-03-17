13072f62b503d2104339ba27ca20a2572dae757a
Christian Kaestner <kaestner@informatik.uni-marburg.de>
2011-03-17 17:47:15 +0100

    dont preprocess non-existing files

--- linuxParseTypecheck.sh
+++ linuxParseTypecheck.sh
@@ -2,10 +2,14 @@
 . linuxFileList.inc
 
 filesToProcess|while read i; do
-  if [ ! -f $srcPath/$i.pi.dbgT ]; then
-    touch $srcPath/$i.pi.dbgT
-    ./parseTypecheck.sh $srcPath/$i.pi
+  if [ -f $srcPath/$i.pi ]; then
+      if [ ! -f $srcPath/$i.pi.dbgT ]; then
+        touch $srcPath/$i.pi.dbgT
+        ./parseTypecheck.sh $srcPath/$i.pi
+      else
+        echo "Skipping $srcPath/$i.pi"
+      fi
   else
-    echo "Skipping $srcPath/$i.pi"
+    echo "File $srcPath/$i.pi not found."
   fi
 done
