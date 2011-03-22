8eae2928c4c6b50e9f18b28794d5174739d3b4e6
Christian Kaestner <kaestner@informatik.uni-marburg.de>
2011-03-22 09:23:20 +0100

    debugging update

--- LinuxAnalysis/src/main/scala/de/fosd/typechef/linux/LinuxDependencyAnalysis.scala
+++ LinuxAnalysis/src/main/scala/de/fosd/typechef/linux/LinuxDependencyAnalysis.scala
@@ -12,7 +12,7 @@ import de.fosd.typechef.featureexpr.FeatureExpr._
 
 object LinuxDependencyAnalysis {
 
-    import LinuxFeatureModel.featureModel
+    import LinuxFeatureModel.featureModelExcludingDead
 
     def main(args: Array[String]): Unit = {
         val featureNames = List(
@@ -23,19 +23,24 @@ object LinuxDependencyAnalysis {
             //            "CONFIG_X86_PAE",
             //            "CONFIG_X86_IO_APIC", "CONFIG_ACPI",
             //            "CONFIG_BLOCK", "CONFIG_BLK_DEV_DRBD","CONFIG_BLK_DEV_INTEGRITY"
-            "CONFIG_AMIGA", "CONFIG_X86", "CONFIG_M32R", "CONFIG_SPARC", "CONFIG_M68K"
-
+            "CONFIG_AMIGA", "CONFIG_X86", "CONFIG_M32R", "CONFIG_SPARC", "CONFIG_M68K", "CONFIG_AMIGA_FLOPPY"
         );
         val features = featureNames.map(FeatureExpr.createDefinedExternal(_))
 
 
-
+        val fm=featureModelExcludingDead
 
         println(features)
+        for (f1 <- features) {
+            if (f1.isTautology(fm))
+                println(f1+ " is tautology")
+            if (f1.isContradiction(fm))
+                println(f1+ " is contradiction")
+        }
         for (f1 <- features; f2 <- features if f1 != f2) {
-            if ((f1 implies f2).isTautology(featureModel))
+            if ((f1 implies f2).isTautology(fm))
                 println(f1 + " => " + f2)
-            if ((f1 mex f2).isTautology(featureModel))
+            if ((f1 mex f2).isTautology(fm))
                 println(f1 + " mex " + f2)
             //            println(f1 + " boh " + f2)
         }
