ad3cda3c28b046388763e518863a57b3cf34b266
Christian Kaestner <kaestner@informatik.uni-marburg.de>
2011-04-01 08:33:32 +0200

    additional constraints for the linux feature model

--- LinuxAnalysis/src/main/scala/de/fosd/typechef/linux/LinuxDependencyAnalysis.scala
+++ LinuxAnalysis/src/main/scala/de/fosd/typechef/linux/LinuxDependencyAnalysis.scala
@@ -12,7 +12,6 @@ import de.fosd.typechef.featureexpr.FeatureExpr._
 
 object LinuxDependencyAnalysis {
 
-    import LinuxFeatureModel.featureModelExcludingDead
 
     def main(args: Array[String]): Unit = {
         val featureNames = List(
@@ -22,14 +21,21 @@ object LinuxDependencyAnalysis {
             //            "CONFIG_DISCONTIGMEM", "CONFIG_FLATMEM", "CONFIG_SPARSEMEM",
             //            "CONFIG_X86_PAE",
             //            "CONFIG_X86_IO_APIC", "CONFIG_ACPI",
-            //            "CONFIG_BLOCK", "CONFIG_BLK_DEV_DRBD","CONFIG_BLK_DEV_INTEGRITY"
+            //                        "CONFIG_BLOCK","CONFIG_EXT3_FS","CONFIG_EXT4_FS"    ,"CONFIG_JBD" ,"CONFIG_JBD2"
+            //        "CONFIG_IA32_EMULATION","CONFIG_64BIT",
+            //"CONFIG_X86_32" ,
+            //"CONFIG_X86_64",
+            //"CONFIG_X86"  ,
+            "CONFIG_PARAVIRT", "CONFIG_PROC_FS", "CONFIG_TRACE_IRQFLAGS_SUPPORT", "CONFIG_X86_PAE"
+            // "CONFIG_BLK_DEV_DRBD","CONFIG_BLK_DEV_INTEGRITY", "CONFIG_BLK_DEV_RAM" , "CONFIG_BLK_DEV_LOOP"
             //            "CONFIG_AMIGA", "CONFIG_X86", "CONFIG_M32R", "CONFIG_SPARC", "CONFIG_M68K", "CONFIG_AMIGA_FLOPPY"
-            "CONFIG_BLOCK", "CONFIG_PS3_DISK", "CONFIG_PPC_PS3"
+            //            "CONFIG_BLOCK", "CONFIG_PS3_DISK", "CONFIG_PPC_PS3"
+            //        "CONFIG_USB", "CONFIG_USB_LIBUSUAL"
         );
         val features = featureNames.map(FeatureExpr.createDefinedExternal(_))
 
 
-        val fm = featureModelExcludingDead
+        val fm = LinuxFeatureModel.featureModel
 
         println(features)
         for (f1 <- features) {
