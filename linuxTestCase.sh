ca0015a606dfca8f07d34194ddb596a90753ede3
Paolo Giarrusso <p.giarrusso@gmail.com>
2011-04-05 00:44:22 +0200

    linuxTestCase.sh: fix quoting for KBUILD_* macros, so that their output is actually quoted
    Fixes drivers/edac/amd76x_edac and others.

--- linuxTestCase.sh
+++ linuxTestCase.sh
@@ -86,7 +86,7 @@ flags() {
   fi
   # XXX: again, I need to specify $PWD, for the same bug as above.
   # "-I linux-2.6.33.3/include -I linux-2.6.33.3/arch/x86/include"
-  echo "$extraFlag -I $srcPath/include -I $srcPath/arch/x86/include -D __KERNEL__ -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -DKBUILD_BASENAME=\"$base\" -DKBUILD_MODNAME=\"$base\""
+  echo "$extraFlag -I $srcPath/include -I $srcPath/arch/x86/include -D __KERNEL__ -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -DKBUILD_BASENAME=\"\\\"$base\\\"\" -DKBUILD_MODNAME=\"\\\"$base\\\"\""
 }
 
 export outCSV=linux.csv
