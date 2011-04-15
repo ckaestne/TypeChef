2fe783a8a8ce454a5962ec50f8505a61743ae2b9
Christian Kaestner <kaestner@informatik.uni-marburg.de>
2011-04-15 19:42:16 +0200

    add new junit report facility for hudson

--- project/build/TypeChef.scala
+++ project/build/TypeChef.scala
@@ -1,5 +1,6 @@
 import sbt._
 import reaktor.scct.ScctProject
+import eu.henkelmann.sbt.JUnitXmlTestsListener
 
 class TypeChef(info: ProjectInfo) extends ParentProject(info) with IdeaProject {
 
@@ -17,6 +18,8 @@ class TypeChef(info: ProjectInfo) extends ParentProject(info) with IdeaProject {
         val junit = "junit" % "junit" % "4.8.2" % "test->default"
         val junitInterface = "com.novocode" % "junit-interface" % "0.5" % "test->default"
         val scalacheck = "org.scala-tools.testing" % "scalacheck_2.8.1" % "1.8" % "test->default"
+	def junitXmlListener: TestReportListener = new JUnitXmlTestsListener(outputPath.toString)
+	override def testListeners: Seq[TestReportListener] = super.testListeners ++ Seq(junitXmlListener)
 
         override def javaCompileOptions = super.javaCompileOptions ++ javaCompileOptions("-source", "1.5", "-Xlint:unchecked")
         override def compileOptions = super.compileOptions ++ Seq(Unchecked,
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -5,4 +5,6 @@ class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
     lazy val scctPlugin = "reaktor" % "sbt-scct-for-2.8" % "0.1-SNAPSHOT"
     val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
     val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.2.0"
+    val junitXmlRepo = "Christoph's Maven Repo" at "http://maven.henkelmann.eu/"
+    val junitXml = "eu.henkelmann" % "junit_xml_listener" % "0.2"
 }
