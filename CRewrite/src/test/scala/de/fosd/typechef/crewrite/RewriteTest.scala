c5216d1602e3d7d8f8f477491e16e62ab2b875e4
Christian Kaestner <kaestner@informatik.uni-marburg.de>
2011-06-09 12:01:41 +0200

    explore possibilities to rewrite AST (incomplete)

--- CRewrite/src/test/scala/de/fosd/typechef/crewrite/RewriteTest.scala
+++ CRewrite/src/test/scala/de/fosd/typechef/crewrite/RewriteTest.scala
@@ -1,11 +1,10 @@
 package de.fosd.typechef.crewrite
 
 import org.junit.Test
-import de.fosd.typechef.parser.c.TestHelper
 import de.fosd.typechef.parser.c._
 import de.fosd.typechef.parser.Opt
 import org.kiama.attribution.Attributable
-   import org.scalatest.junit.JUnitRunner
+import org.scalatest.junit.JUnitRunner
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FunSuite
 import org.junit.runner.RunWith
@@ -16,7 +15,7 @@ class RewriteTest extends FunSuite with ShouldMatchers with TestHelper {
 
     @Test
     def eliminateVariability() {
-        val ast= getAST("""
+        val ast = getAST("""
         int foo() {
             #ifdef X
             foo();
@@ -26,7 +25,7 @@ class RewriteTest extends FunSuite with ShouldMatchers with TestHelper {
         }
         """)
 
-        val newast=new IfdefToIf().rewrite(ast)
+        val newast = new IfdefToIf().rewrite(ast)
 
         assertNotVariability(newast)
 
@@ -34,44 +33,44 @@ class RewriteTest extends FunSuite with ShouldMatchers with TestHelper {
     }
 
 
-    private def assertNotVariability(ast:Attributable) {
+    private def assertNotVariability(ast: Attributable) {
         ast match {
-            case Opt(f,_) => assert(f.isTautology,"Optional children not expected: "+ast)
-            case c:Choice[_] => assert(false,"Choice nodes not expected: "+ast)
-            case _=>
+            case Opt(f, _) => assert(f.isTautology, "Optional children not expected: " + ast)
+            case c: Choice[_] => assert(false, "Choice nodes not expected: " + ast)
+            case _ =>
         }
 
-        for (c<-ast.children)
+        for (c <- ast.children)
             assertNotVariability(c)
 
 
     }
 
-    test("rewrite variability granularity") {
-       val ast= getAST("""
-        int foo() {
-            foo(
-            #ifdef X
-                a
-            #endif
-            );
-            static
-            #ifdef Y
-            long
-            #endif
-            int a=3;
-        }
-        """)
-        println(ast)
-
-        val cv=new CoarseVariability()
-        cv.check(ast) should be (false)
-
-        val newast=cv.rewrite(ast)
-        cv.check(newast) should be (true)
-
-
-
-    }
+    //    test("rewrite variability granularity") {
+    //       val ast= getAST("""
+    //        int foo() {
+    //            foo(
+    //            #ifdef X
+    //                a
+    //            #endif
+    //            );
+    //            static
+    //            #ifdef Y
+    //            long
+    //            #endif
+    //            int a=3;
+    //        }
+    //        """)
+    //        println(ast)
+    //
+    //        val cv=new CoarseVariability()
+    //        cv.check(ast) should be (false)
+    //
+    //        val newast=cv.rewrite(ast)
+    //        cv.check(newast) should be (true)
+    //
+    //
+    //
+    //    }
 
 }
