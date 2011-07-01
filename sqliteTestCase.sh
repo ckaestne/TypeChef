edd77987e5c5e63953f2b4121ffd293b5ababc7d
Jörg Liebig <joliebig@fim.uni-passau.de>
2011-07-01 15:28:58 +0200

    removing *.h from testcases

--- sqliteTestCase.sh
+++ sqliteTestCase.sh
@@ -5,21 +5,22 @@
 
 srcPath=./casestudies/sqlite-3.6.10/src
 prjPath=./casestudies/sqlite-3.6.10
-export partialPreprocFlags="-p false \
-	-U HAVE_LOCALTIME_S\
-	-U _FILE_OFFSET_BITS\
-	-U _LARGE_FILES\
-	--openFeat $prjPath/sqlitefeatures.txt\
-"
-flags="\
-	-I $prjPath -I $srcPath\
-	--include ./host/platform.h\
-	-I /usr/local/include\
-	-I /usr/lib/x86_64-linux-gnu/gcc/x86_64-linux-gnu/4.5.2/include\
-	-I /usr/lib/x86_64-linux-gnu/gcc/x86_64-linux-gnu/4.5.2/include-fixed\
-	-I /usr/include/x86_64-linux-gnu\
-	-I /usr/include\
-"
+export partialPreprocFlags='-p true
+	-U HAVE_LOCALTIME_S
+	-U _FILE_OFFSET_BITS
+	-U _LARGE_FILES
+	--openFeat '$prjPath'/sqlitefeatures.txt
+'
+flags='
+	-I '$prjPath'
+	-I '$srcPath'
+	--include host/platform.h
+	-I /usr/local/include
+	-I /usr/lib/x86_64-linux-gnu/gcc/x86_64-linux-gnu/4.5.2/include
+	-I /usr/lib/x86_64-linux-gnu/gcc/x86_64-linux-gnu/4.5.2/include-fixed
+	-I /usr/include/x86_64-linux-gnu
+	-I /usr/include
+'
 
 ##################################################################
 # Actually invoke the preprocessor and analyze result.
