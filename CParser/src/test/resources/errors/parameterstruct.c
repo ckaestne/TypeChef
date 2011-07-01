bc3407358dafebd3b5fd0c3fb4df0564dc1c5875
Jörg Liebig <joliebig@fim.uni-passau.de>
2011-07-01 15:27:05 +0200

    adding testcase for parser function parameter is an array with a given size.

---
+++ CParser/src/test/resources/errors/parameterstruct.c
@@ -0,0 +1,12 @@
+struct test {
+    int a;
+    int b;
+}
+
+struct test2 {
+    int c;
+    int d;
+}
+
+int getaddrinfo_a (int __mode, struct test *__list[20],
+        int __ent, struct test2 *__restrict __sig);
