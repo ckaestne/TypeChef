691c4115a4482dc2a99369cd2fb291555dc0751c
Christian Kaestner <kaestner@informatik.uni-marburg.de>
2011-07-01 19:15:50 +0200

    remove unnecessary curly braces

--- CParser/src/main/scala/de/fosd/typechef/parser/c/AST.scala
+++ CParser/src/main/scala/de/fosd/typechef/parser/c/AST.scala
@@ -74,35 +74,27 @@ abstract class PostfixSuffix extends AST
 
 case class SimplePostfixSuffix(t: String) extends PostfixSuffix
 
-case class PointerPostfixSuffix(kind: String, id: Id) extends PostfixSuffix {
-}
+case class PointerPostfixSuffix(kind: String, id: Id) extends PostfixSuffix
 
-case class FunctionCall(params: ExprList) extends PostfixSuffix {
-}
+case class FunctionCall(params: ExprList) extends PostfixSuffix
 
-case class ArrayAccess(expr: Expr) extends PostfixSuffix {
-}
+case class ArrayAccess(expr: Expr) extends PostfixSuffix
 
 case class PostfixExpr(p: Expr, s: PostfixSuffix) extends Expr
 
-case class UnaryExpr(kind: String, e: Expr) extends Expr {
-}
+case class UnaryExpr(kind: String, e: Expr) extends Expr
 
-case class SizeOfExprT(typeName: TypeName) extends Expr {
-}
+case class SizeOfExprT(typeName: TypeName) extends Expr
 
-case class SizeOfExprU(expr: Expr) extends Expr {
-}
+case class SizeOfExprU(expr: Expr) extends Expr
 
-case class CastExpr(typeName: TypeName, expr: Expr) extends Expr {
-}
+case class CastExpr(typeName: TypeName, expr: Expr) extends Expr
 
 case class PointerDerefExpr(castExpr: Expr) extends Expr
 
 case class PointerCreationExpr(castExpr: Expr) extends Expr
 
-case class UnaryOpExpr(kind: String, castExpr: Expr) extends Expr {
-}
+case class UnaryOpExpr(kind: String, castExpr: Expr) extends Expr
 
 case class NAryExpr(e: Expr, others: List[Opt[NArySubExpr]]) extends Expr
 
@@ -117,49 +109,37 @@ case class ExprList(exprs: List[Opt[Expr]]) extends Expr
 //Statements
 abstract sealed class Statement extends AST
 
-case class CompoundStatement(innerStatements: List[Opt[Statement]]) extends Statement {
-}
+case class CompoundStatement(innerStatements: List[Opt[Statement]]) extends Statement
 
 case class EmptyStatement() extends Statement
 
-case class ExprStatement(expr: Expr) extends Statement {
-}
+case class ExprStatement(expr: Expr) extends Statement
 
-case class WhileStatement(expr: Expr, s: Conditional[Statement]) extends Statement {
-}
+case class WhileStatement(expr: Expr, s: Conditional[Statement]) extends Statement
 
-case class DoStatement(expr: Expr, s: Conditional[Statement]) extends Statement {
-}
+case class DoStatement(expr: Expr, s: Conditional[Statement]) extends Statement
 
-case class ForStatement(expr1: Option[Expr], expr2: Option[Expr], expr3: Option[Expr], s: Conditional[Statement]) extends Statement {
-}
+case class ForStatement(expr1: Option[Expr], expr2: Option[Expr], expr3: Option[Expr], s: Conditional[Statement]) extends Statement
 
-case class GotoStatement(target: Expr) extends Statement {
-}
+case class GotoStatement(target: Expr) extends Statement
 
 case class ContinueStatement() extends Statement
 
 case class BreakStatement() extends Statement
 
-case class ReturnStatement(expr: Option[Expr]) extends Statement {
-}
+case class ReturnStatement(expr: Option[Expr]) extends Statement
 
-case class LabelStatement(id: Id, attribute: Option[AttributeSpecifier]) extends Statement {
-}
+case class LabelStatement(id: Id, attribute: Option[AttributeSpecifier]) extends Statement
 
-case class CaseStatement(c: Expr, s: Option[Conditional[Statement]]) extends Statement {
-}
+case class CaseStatement(c: Expr, s: Option[Conditional[Statement]]) extends Statement
 
-case class DefaultStatement(s: Option[Conditional[Statement]]) extends Statement {
-}
+case class DefaultStatement(s: Option[Conditional[Statement]]) extends Statement
 
 case class IfStatement(condition: Expr, thenBranch: Conditional[Statement], elifs: List[Opt[ElifStatement]], elseBranch: Option[Conditional[Statement]]) extends Statement
 
-case class ElifStatement(condition: Expr, thenBranch: Conditional[Statement]) extends AST {
-}
+case class ElifStatement(condition: Expr, thenBranch: Conditional[Statement]) extends AST
 
-case class SwitchStatement(expr: Expr, s: Conditional[Statement]) extends Statement {
-}
+case class SwitchStatement(expr: Expr, s: Conditional[Statement]) extends Statement
 
 abstract sealed class CompoundDeclaration extends Statement
 
@@ -224,22 +204,18 @@ abstract class Attribute() extends AST
 
 case class AtomicAttribute(n: String) extends Attribute
 
-case class AttributeSequence(attributes: List[Opt[Attribute]]) extends AST {
-}
+case class AttributeSequence(attributes: List[Opt[Attribute]]) extends AST
 
-case class CompoundAttribute(inner: List[Opt[AttributeSequence]]) extends Attribute {
-}
+case class CompoundAttribute(inner: List[Opt[AttributeSequence]]) extends Attribute
 
 case class Declaration(declSpecs: List[Opt[Specifier]], init: List[Opt[InitDeclarator]]) extends ExternalDef with OldParameterDeclaration
 
 
 abstract class InitDeclarator(val declarator: Declarator, val attributes: List[Opt[AttributeSpecifier]]) extends AST {def getName = declarator.getName;}
 
-case class InitDeclaratorI(override val declarator: Declarator, override val attributes: List[Opt[AttributeSpecifier]], i: Option[Initializer]) extends InitDeclarator(declarator, attributes) {
-}
+case class InitDeclaratorI(override val declarator: Declarator, override val attributes: List[Opt[AttributeSpecifier]], i: Option[Initializer]) extends InitDeclarator(declarator, attributes)
 
-case class InitDeclaratorE(override val declarator: Declarator, override val attributes: List[Opt[AttributeSpecifier]], e: Expr) extends InitDeclarator(declarator, attributes) {
-}
+case class InitDeclaratorE(override val declarator: Declarator, override val attributes: List[Opt[AttributeSpecifier]], e: Expr) extends InitDeclarator(declarator, attributes)
 
 
 /**
@@ -312,14 +288,11 @@ case class VarArgs() extends ParameterDeclaration(List()) with OldParameterDecla
 
 case class EnumSpecifier(id: Option[Id], enumerators: Option[List[Opt[Enumerator]]]) extends TypeSpecifier
 
-case class Enumerator(id: Id, assignment: Option[Expr]) extends AST {
-}
+case class Enumerator(id: Id, assignment: Option[Expr]) extends AST
 
-case class StructOrUnionSpecifier(isUnion: Boolean, id: Option[Id], enumerators: List[Opt[StructDeclaration]]) extends TypeSpecifier {
-}
+case class StructOrUnionSpecifier(isUnion: Boolean, id: Option[Id], enumerators: List[Opt[StructDeclaration]]) extends TypeSpecifier
 
-case class StructDeclaration(qualifierList: List[Opt[Specifier]], declaratorList: List[Opt[StructDecl]]) extends AST {
-}
+case class StructDeclaration(qualifierList: List[Opt[Specifier]], declaratorList: List[Opt[StructDecl]]) extends AST
 
 sealed abstract class StructDecl extends AST
 
@@ -327,8 +300,7 @@ case class StructDeclarator(decl: Declarator, initializer: Option[Expr], attribu
 
 case class StructInitializer(expr: Expr, attributes: List[Opt[AttributeSpecifier]]) extends StructDecl
 
-case class AsmExpr(isVolatile: Boolean, expr: Expr) extends AST with ExternalDef {
-}
+case class AsmExpr(isVolatile: Boolean, expr: Expr) extends AST with ExternalDef
 
 case class FunctionDef(specifiers: List[Opt[Specifier]], declarator: Declarator, oldStyleParameters: List[Opt[OldParameterDeclaration]], stmt: CompoundStatement) extends AST with ExternalDef {
     def getName = declarator.getName
@@ -338,46 +310,34 @@ trait ExternalDef extends AST
 
 case class EmptyExternalDef() extends ExternalDef
 
-case class TypelessDeclaration(declList: List[Opt[InitDeclarator]]) extends ExternalDef {
-}
+case class TypelessDeclaration(declList: List[Opt[InitDeclarator]]) extends ExternalDef
 
 
-case class TypeName(specifiers: List[Opt[Specifier]], decl: Option[AbstractDeclarator]) extends AST {
-}
+case class TypeName(specifiers: List[Opt[Specifier]], decl: Option[AbstractDeclarator]) extends AST
 
-case class TranslationUnit(defs: List[Opt[ExternalDef]]) extends AST {
-}
+case class TranslationUnit(defs: List[Opt[ExternalDef]]) extends AST
 
 //GnuC stuff here:
 abstract class AttributeSpecifier() extends Specifier()
 
-case class GnuAttributeSpecifier(attributeList: List[Opt[AttributeSequence]]) extends AttributeSpecifier {
-}
+case class GnuAttributeSpecifier(attributeList: List[Opt[AttributeSequence]]) extends AttributeSpecifier
 
-case class AsmAttributeSpecifier(stringConst: StringLit) extends AttributeSpecifier {
-}
+case class AsmAttributeSpecifier(stringConst: StringLit) extends AttributeSpecifier
 
-case class LcurlyInitializer(inits: List[Opt[Initializer]]) extends Expr {
-}
+case class LcurlyInitializer(inits: List[Opt[Initializer]]) extends Expr
 
-case class AlignOfExprT(typeName: TypeName) extends Expr {
-}
+case class AlignOfExprT(typeName: TypeName) extends Expr
 
-case class AlignOfExprU(expr: Expr) extends Expr {
-}
+case class AlignOfExprU(expr: Expr) extends Expr
 
-case class GnuAsmExpr(isVolatile: Boolean, expr: StringLit, stuff: Any) extends Expr {
-}
+case class GnuAsmExpr(isVolatile: Boolean, expr: StringLit, stuff: Any) extends Expr
 
-case class RangeExpr(from: Expr, to: Expr) extends Expr {
-}
+case class RangeExpr(from: Expr, to: Expr) extends Expr
 
 
-case class TypeOfSpecifierT(typeName: TypeName) extends TypeSpecifier {
-}
+case class TypeOfSpecifierT(typeName: TypeName) extends TypeSpecifier
 
-case class TypeOfSpecifierU(expr: Expr) extends TypeSpecifier {
-}
+case class TypeOfSpecifierU(expr: Expr) extends TypeSpecifier
 
 
 abstract class InitializerElementLabel() extends AST
@@ -391,8 +351,7 @@ case class InitializerDesignatorD(id: Id) extends InitializerElementLabel
 case class InitializerAssigment(designators: List[Opt[InitializerElementLabel]]) extends InitializerElementLabel
 
 
-case class BuiltinOffsetof(typeName: TypeName, offsetofMemberDesignator: List[Opt[OffsetofMemberDesignator]]) extends PrimaryExpr {
-}
+case class BuiltinOffsetof(typeName: TypeName, offsetofMemberDesignator: List[Opt[OffsetofMemberDesignator]]) extends PrimaryExpr
 
 abstract class OffsetofMemberDesignator() extends AST
 
@@ -400,15 +359,11 @@ case class OffsetofMemberDesignatorID(id: Id) extends OffsetofMemberDesignator
 
 case class OffsetofMemberDesignatorExpr(expr: Expr) extends OffsetofMemberDesignator
 
-case class BuiltinTypesCompatible(typeName1: TypeName, typeName2: TypeName) extends PrimaryExpr {
-}
+case class BuiltinTypesCompatible(typeName1: TypeName, typeName2: TypeName) extends PrimaryExpr
 
-case class BuiltinVaArgs(expr: Expr, typeName: TypeName) extends PrimaryExpr {
-}
+case class BuiltinVaArgs(expr: Expr, typeName: TypeName) extends PrimaryExpr
 
-case class CompoundStatementExpr(compoundStatement: CompoundStatement) extends PrimaryExpr {
-}
+case class CompoundStatementExpr(compoundStatement: CompoundStatement) extends PrimaryExpr
 
-case class Pragma(command: StringLit) extends ExternalDef {
-}
+case class Pragma(command: StringLit) extends ExternalDef 
 
