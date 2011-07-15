ec26d8bd9469c518e51608b3999f0bf74b5f4b3e
Jörg Liebig <joliebig@fim.uni-passau.de>
2011-07-15 16:54:05 +0200

    adding navigation support between Opt elements

--- CTypeChecker/src/main/scala/de/fosd/typechef/typesystem/ASTNavigation.scala
+++ CTypeChecker/src/main/scala/de/fosd/typechef/typesystem/ASTNavigation.scala
@@ -6,67 +6,133 @@ import org.kiama.attribution.Attribution._
 import org.kiama._
 import attribution.Attributable
 import de.fosd.typechef.parser.{Conditional, One, Opt, Choice}
+import javax.management.remote.rmi._RMIConnection_Stub
 
 /**
  * Simplified navigation support
  *
- * prevAST and parentAST provide navigation between
+ * prevAST, nextAST, and parentAST provide navigation between
  * AST nodes not affected by Opt and Choice nodes
+ * (those are just flattened)
+ *
+ * prevOpt, nextOpt, and parentOpt provide navigation between
+ * Opt nodes not affected by AST nodes
  * (those are just flattened)
  */
 trait ASTNavigation {
 
-    val parentAST: Attributable ==> AST = attr {case a: Attributable => findParent(a)}
-    private def findParent(a: Attributable): AST =
-        a.parent match {
-            case o: Opt[_] => findParent(o)
-            case c: Conditional[_] => findParent(c)
-            case a: AST => a
-            case _ => null
-        }
+  val DEBUG = true
 
-    val prevAST: Attributable ==> AST = attr {
-        case a =>
-            a.prev[Attributable] match {
-                case c: Choice[AST] => lastChoice(c)
-                case o: One[AST] => o.value
-                case a: AST => a
-                case Opt(_, v: Choice[AST]) => lastChoice(v)
-                case Opt(_, v: One[AST]) => v.value
-                case Opt(_, v: AST) => v
-                case null => {
-                    a.parent match {
-                        case o: Opt[_] => o -> prevAST
-                        case c: Choice[AST] => c -> prevAST
-                        case c: One[AST] => c -> prevAST
-                        case _ => null
-                    }
-                }
-            }
+  val parentOpt: Attributable ==> Attributable = attr {case a: Attributable => findParentOpt(a)}
+  // return type is too generic; Opt and Choice preferable
+  private def findParentOpt(a: Attributable): Attributable = {
+    a.parent match {
+      case o: Opt[Attributable] if (!o.feature.isTautology()) => o
+      case c: Choice[Attributable] if (!c.feature.isTautology()) => c
+      case a: AST => findParentOpt(a)
+      case _ => null
+    }
+  }
+
+  val prevOpt: Attributable ==> Attributable = attr {
+    case a =>
+      a.prev[Attributable] match {
+        case c: Conditional[_] => null
+        case o: Opt[_] if (o.feature.isTautology) => null
+        case o: Opt[_] if (!o.feature.isTautology) => {
+          a match {
+            case no: Opt[Attributable] if (o.feature.equals(no.feature)) => o
+            case _ => null
+          }
+        }
+        case null => null
+        case _ => assert(false, "cannot call prevOpt on instances other than Choice, One, or Opt"); null
+      }
+  }
+
+  val nextOpt: Attributable ==> Attributable = attr {
+    case a =>
+      a.next[Attributable] match {
+        case c: Conditional[_] => null
+        case o: Opt[_] if (o.feature.isTautology) => null
+        case o: Opt[_] if (!o.feature.isTautology) => {
+          a match {
+            case po: Opt[_] if (o.feature.equals(po.feature)) => o
+            case _ => null
+          }
+        }
+        case null => null
+        case _ => assert(false, "cannot call nextOpt on instances other than Choice, One, or Opt"); null
+      }
+  }
+
+  val isVariable: Attributable ==> Boolean = attr {
+    case a =>
+      a match {
+        case _: Conditional[_] => true
+        case o: Opt[_] if (o.feature.isTautology) => a.parent[Attributable]->isVariable
+        case o: Opt[_] if (!o.feature.isTautology) => true
+        case e: AST if (a.isRoot) => false
+        case e: AST => a.parent[Attributable]->isVariable
+        case _ => assert(false, "invalid element"); false
+      }
+  }
+
+  val parentAST: Attributable ==> AST = attr {case a: Attributable => findParent(a)}
+  private def findParent(a: Attributable): AST =
+    a.parent match {
+      case o: Opt[_] => findParent(o)
+      case c: Conditional[_] => findParent(c)
+      case a: AST => a
+      case _ => null
     }
 
-    /**try first prev and if that does not exist, then parent*/
-    val prevOrParentAST: Attributable ==> AST = {case a: Attributable => val p = a -> prevAST; if (p != null) p else a -> parentAST}
-
-    private def prevOfChoice(c: Choice[AST]): AST = c -> prevAST match {
-        case x: Choice[AST] => lastChoice(x)
-        case x: AST => x
-        case null => c.parent match {
-            case x: Choice[AST] => prevOfChoice(x)
+  val prevAST: Attributable ==> AST = attr {
+    case a =>
+      (a.prev[Attributable]: @unchecked) match {
+        case c: Choice[AST] => lastChoice(c)
+        case o: One[AST] => o.value
+        case a: AST => a
+        case Opt(_, v: Choice[AST]) => lastChoice(v)
+        case Opt(_, v: One[AST]) => v.value
+        case Opt(_, v: AST) => v
+        case null => {
+          a.parent match {
+            case o: Opt[_] => o -> prevAST
+            case c: Choice[AST] => c -> prevAST
+            case c: One[AST] => c -> prevAST
             case _ => null
+          }
         }
+      }
+  }
+
+  /**try first prev and if that does not exist, then parent*/
+  val prevOrParentAST: Attributable ==> AST = {case a: Attributable => val p = a -> prevAST; if (p != null) p else a -> parentAST}
+
+  private def prevOfChoice(c: Choice[AST]): AST = c -> prevAST match {
+    case x: Choice[AST] => lastChoice(x)
+    case x: AST => x
+    case null => c.parent match {
+      case x: Choice[AST] => prevOfChoice(x)
+      case _ => null
+    }
+  }
+
+  private def lastChoice[T <: AST](x: Choice[T]): T =
+    x.elseBranch match {
+      case c: Choice[T] => lastChoice(c)
+      case One(c) => c
     }
 
+  private def firstChoice[T <: AST](x: Choice[T]): T =
+    x.thenBranch match {
+      case c: Choice[T] => firstChoice(c)
+      case One(c) => c
+    }
 
-    private def lastChoice[T <: AST](x: Choice[T]): T =
-        x.elseBranch match {
-            case c: Choice[T] => lastChoice(c)
-            case One(c) => c
-        }
-
-
-    protected def outer[T](f: AST ==> T, init: () => T, e: AST): T =
-        if (e -> prevOrParentAST != null) f(e -> prevOrParentAST)
-        else
-            init()
+  protected def outer[T](f: AST ==> T, init: () => T, e: AST): T =
+    if (e -> prevOrParentAST != null) f(e -> prevOrParentAST)
+    else
+      init()
 }
--- CTypeChecker/src/test/scala/de/fosd/typechef/typesystem/ASTNavigationTest.scala
+++ CTypeChecker/src/test/scala/de/fosd/typechef/typesystem/ASTNavigationTest.scala
@@ -40,7 +40,104 @@ class ASTNavigationTest extends FunSuite with ShouldMatchers with ASTNavigation 
     private val fa = FeatureExpr.createDefinedExternal("a")
     private val fb = FeatureExpr.createDefinedExternal("b")
     private val fc = FeatureExpr.createDefinedExternal("c")
+    private val fd = FeatureExpr.createDefinedExternal("d")
+    private val fe = FeatureExpr.createDefinedExternal("e")
+    private val ff = FeatureExpr.createDefinedExternal("f")
+    private val fg = FeatureExpr.createDefinedExternal("g")
     private def o[T](o: T) = Option(base, o)
+
+    test("ast navigation (parent) with Opt and Choice (tree)") {
+        implicit def toOne[T](x: T): Conditional[T] = One(x)
+        val stmt0 = LabelStatement(Id("stmt0"), None)
+        val stmt1 = LabelStatement(Id("stmt1"), None)
+        val stmt2 = LabelStatement(Id("stmt2"), None)
+        val stmt3 = LabelStatement(Id("stmt3"), None)
+        val stmt4 = LabelStatement(Id("stmt4"), None)
+        val stmt5 = LabelStatement(Id("stmt5"), None)
+        val stmt6 = LabelStatement(Id("stmt6"), None)
+        val stmt7 = LabelStatement(Id("stmt7"), None)
+        val c1 = Choice(fb, stmt4, stmt5)
+        val c2 = Choice(fb, stmt2, stmt3)
+        val c3 = Choice(fa, c2, c1)
+        val c4 = Choice(fc, stmt1, c3)
+        //val root = CompoundStatement(Conditional.flatten(List[Conditional[Statement]](stmt0, c4, stmt6, stmt7).map(Opt(base, _))))
+
+        c4 -> parentOpt should equal(null)
+        c3 -> parentOpt should equal(c4)
+        c2 -> parentOpt should equal(c3)
+        c3 -> parentOpt -> parentOpt should equal(null)
+        c1 -> parentOpt -> parentOpt -> parentOpt should equal(null)
+        c2 -> parentOpt -> parentOpt -> parentOpt should equal(null)
+    }
+
+    test("ast navigation (prev and next) with Opt and Choice (tree)") {
+      implicit def toOne[T](x: T): Conditional[T] = One(x)
+      val stmt0 = LabelStatement(Id("stmt0"), None)
+      val stmt1 = LabelStatement(Id("stmt1"), None)
+      val stmt2 = LabelStatement(Id("stmt2"), None)
+      val stmt3 = LabelStatement(Id("stmt3"), None)
+      val stmt4 = LabelStatement(Id("stmt4"), None)
+      val stmt5 = LabelStatement(Id("stmt5"), None)
+      val stmt6 = LabelStatement(Id("stmt6"), None)
+      val stmt7 = LabelStatement(Id("stmt7"), None)
+      val stmt8 = LabelStatement(Id("stmt8"), None)
+      val stmt9 = LabelStatement(Id("stmt9"), None)
+      val optstmt0 = Opt(fd, stmt0)
+      val optstmt1 = Opt(fd, stmt1)
+      val optstmt2 = Opt(fd, stmt2)
+      val optstmt3 = Opt(fd, stmt3)
+      val optstmt4 = Opt(fd, stmt4)
+      val optstmt5 = Opt(fd, stmt5)
+      val optstmt6 = Opt(fd, stmt6)
+      val optstmt7 = Opt(fd, stmt7)
+      val optstmt8 = Opt(FeatureExpr.base, stmt8)
+      val optstmt9 = Opt(FeatureExpr.base, stmt9)
+
+      val c1 = Choice(fb, stmt4, stmt5)
+      val c2 = Choice(fb, stmt2, stmt3)
+      val c3 = Choice(fa, c2, c1)
+      val c4 = Choice(fc, stmt1, c3)
+      val optc4 = Opt(fd, c4)
+      val l = List[Opt[Statement]](optstmt0) ++
+          Conditional.flatten(List(optc4)) ++
+          List[Opt[Statement]](optstmt6, optstmt7, optstmt8, optstmt9)
+
+      val root = CompoundStatement(l)
+      println(PrettyPrinter.print(root))
+
+      //#if fd
+      //stmt0
+      //#if fc // c4
+      //stmt1
+      //#else
+      //  #if fa // c3
+      //    #if fb // c2
+      //    stmt2
+      //    #else
+      //    stmt3
+      //    #endif
+      //  #else
+      //    #if fb // c1
+      //    stmt4
+      //    #else
+      //    stmt5
+      //    #endif
+      //  #endif
+      //#endif
+      //stmt6
+      //stmt7
+      //#endif
+      //stmt8
+      //stmt9
+
+      optstmt7->prevOpt should equal(optstmt6)
+      optstmt6->nextOpt should equal(optstmt7)
+      optstmt7->nextOpt should equal(null)
+      optstmt6->nextOpt->nextOpt should equal(null)
+      optstmt6->prevOpt should equal(null)
+      stmt8->isVariable should equal(false)
+      stmt9->isVariable should equal(false)
+    }
 
     test("ast navigation with Opt") {
         foo -> parentAST should equal(ast)
