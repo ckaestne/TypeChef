51661e12f7e495f9fef5b71191b4d8fa1de84df6
Christian Kaestner <kaestner@informatik.uni-marburg.de>
2011-11-14 22:54:40 +0100

    remove obsolete files

--- CTypeChecker/src/main/scala/de/fosd/typechef/typesystem/CTypeAnalysis.scala
+++
@@ -1,6 +0,0 @@
-//package de.fosd.typechef.typesystem
-//
-///**
-// * bundle for easier import
-// */
-//trait CTypeAnalysis extends CTypes with CDeclTyping with CExprTyping with CTypeEnv with CFunctionRedefinition with CStmtTyping
