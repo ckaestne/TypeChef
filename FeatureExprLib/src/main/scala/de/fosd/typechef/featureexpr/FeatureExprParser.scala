ebd694c2766549fbe5738ee8a0a9fa4187d17b51
Christian Kaestner <kaestner@informatik.uni-marburg.de>
2011-12-29 17:59:08 +0100

    read multiline FExpr files a line at a time (interpreted as conjunction of all lines)

--- FeatureExprLib/src/main/scala/de/fosd/typechef/featureexpr/FeatureExprParser.scala
+++ FeatureExprLib/src/main/scala/de/fosd/typechef/featureexpr/FeatureExprParser.scala
@@ -16,29 +16,45 @@ class FeatureExprParser extends RegexParsers {
 
     //implications
     def expr: Parser[FeatureExpr] =
-        "oneOf" ~ "(" ~> rep1sep(expr, ",") <~ ")" ^^ {e => oneOf(e)} | aterm
+        "oneOf" ~ "(" ~> rep1sep(expr, ",") <~ ")" ^^ {
+            e => oneOf(e)
+        } | aterm
 
     def aterm: Parser[FeatureExpr] =
-        bterm ~ opt("=>" ~> expr) ^^ {case a ~ b => if (b.isDefined) a implies b.get else a}
+        bterm ~ opt("=>" ~> expr) ^^ {
+            case a ~ b => if (b.isDefined) a implies b.get else a
+        }
 
     //mutually exclusion
     def bterm: Parser[FeatureExpr] =
-        cterm ~ opt("<!>" ~> expr) ^^ {case a ~ b => if (b.isDefined) a mex b.get else a}
+        cterm ~ opt("<!>" ~> expr) ^^ {
+            case a ~ b => if (b.isDefined) a mex b.get else a
+        }
 
     //||
     def cterm: Parser[FeatureExpr] =
-        term ~ rep("||" ~> expr) ^^ {case a ~ bs => bs.foldLeft(a)(_ or _)}
+        term ~ rep("||" ~> expr) ^^ {
+            case a ~ bs => bs.foldLeft(a)(_ or _)
+        }
 
     def term: Parser[FeatureExpr] =
-        bool ~ rep("&&" ~> expr) ^^ {case a ~ bs => bs.foldLeft(a)(_ and _)}
+        bool ~ rep("&&" ~> expr) ^^ {
+            case a ~ bs => bs.foldLeft(a)(_ and _)
+        }
 
     def bool: Parser[FeatureExpr] =
         "!" ~> bool ^^ (_ not) |
-                ("(" ~> expr <~ ")") |
-                "InvalidExpression()" ^^ (_ => False) |
-                (("definedEx" | "defined") ~ "(" ~> ID <~ ")") ^^ {toFeature(_)} |
-                "1" ^^ {x => FeatureExpr.base} |
-                "0" ^^ {x => FeatureExpr.dead}
+            ("(" ~> expr <~ ")") |
+            "InvalidExpression()" ^^ (_ => False) |
+            (("definedEx" | "defined") ~ "(" ~> ID <~ ")") ^^ {
+                toFeature(_)
+            } |
+            "1" ^^ {
+                x => FeatureExpr.base
+            } |
+            "0" ^^ {
+                x => FeatureExpr.dead
+            }
 
     def ID = "[A-Za-z0-9_]*".r
 
@@ -46,6 +62,7 @@ class FeatureExprParser extends RegexParsers {
         case Success(r, _) => r
         case NoSuccess(msg, _) => throw new Exception("error parsing " + featureExpr + " " + msg)
     }
+
     def parse(featureExpr: Reader): FeatureExpr = parseAll(expr, featureExpr) match {
         case Success(r, _) => r
         case NoSuccess(msg, _) => throw new Exception("error parsing " + msg)
@@ -54,7 +71,12 @@ class FeatureExprParser extends RegexParsers {
 
     def parseFile(cfilename: String): FeatureExpr = {
         val featureModelFile = new File(cfilename)
-        if (featureModelFile.exists) parseFile(featureModelFile) else FeatureExpr.base
+        if (featureModelFile.exists) {
+            scala.io.Source.fromFile(featureModelFile).getLines().map(line =>
+                if (line.trim.isEmpty) FeatureExpr.base
+                else parse(line)
+            ).fold(FeatureExpr.base)(_ and _)
+        } else FeatureExpr.base
     }
 
     def parseFile(file: File): FeatureExpr =
@@ -63,7 +85,7 @@ class FeatureExprParser extends RegexParsers {
 
     private def oneOf(features: List[FeatureExpr]): FeatureExpr = {
         (for (f1 <- features; f2 <- features if (f1 != f2)) yield f1 mex f2).
-                foldLeft(features.foldLeft(FeatureExpr.dead)(_ or _))(_ and _)
+            foldLeft(features.foldLeft(FeatureExpr.dead)(_ or _))(_ and _)
 
     }
 }
