43ae35e2e0f91011036375637946973159d63dab
Jörg Liebig <joliebig@fim.uni-passau.de>
2012-02-23 16:14:02 +0100

    add missing testfile

---
+++ CRewrite/src/test/resources/testfiles/bug02.c
@@ -0,0 +1,18 @@
+void doprnt_1 (int i) {
+    cclen = (  len );
+    if (zero_flag)
+        pad = '0';
+    pad = ' ';
+#ifdef A
+    if (minlen > cclen && !minus_flag)
+#endif
+    {
+        int to_add = minlen - cclen;
+        while (to_add > 0)
+        {
+            to_add--;
+        }
+    }
+    if (maxlen >= 0)
+        len = 1;
+}
