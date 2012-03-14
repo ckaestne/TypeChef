2c83e8b3d279917cc27430c1759dad856232102b
Jörg Liebig <joliebig@fim.uni-passau.de>
2012-03-14 13:28:08 +0100

    change configuration to a feature expression

--- FeatureExprLib/src/main/scala/de/fosd/typechef/featureexpr/Configuration.scala
+++ FeatureExprLib/src/main/scala/de/fosd/typechef/featureexpr/Configuration.scala
@@ -2,9 +2,9 @@ package de.fosd.typechef.featureexpr
 
 // this class represents the configuration of a product
 // and config holds all defined features
-class Configuration(val config: List[DefinedExternal], fm: FeatureModel = NoFeatureModel) extends SatSolver {
-  val sfm = fm.and(config.fold(FeatureExpr.base)(_ and _))
+class Configuration(val config: FeatureExpr, fm: FeatureModel = NoFeatureModel) extends SatSolver {
+  val sfm = fm and config
   def valid(fexp: FeatureExpr) = isSatisfiable(fexp, sfm)
 }
 
-object EmptyConfiguration extends Configuration(List(), NoFeatureModel)
+object EmptyConfiguration extends Configuration(FeatureExpr.base, NoFeatureModel)
