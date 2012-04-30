ckaestne/TypeChef

commit 74426be70bf8d339c7ff2491b5b20f3bd93ebed2

fix collectDistinctFeatures in definedMacro

FeatureExprLib/src/main/scala/de/fosd/typechef/featureexpr/sat/SATFeatureExpr.scala
@@ -9,7 +9,7 @@ import collection.mutable.Map
 import collection.mutable.WeakHashMap
 import collection.mutable.HashMap
 import collection.mutable.ArrayBuffer
-import scala.Some
+import scala.{Option, Some}
 
 //
 ///**
@@ -990,10 +990,12 @@ class DefinedMacro(val name: String, val presenceCondition: SATFeatureExpr, val 
 
     def feature = name
     override def toTextExpr = "defined(" + name + ")"
-    override def toString = "macro(" + name + ")"
+    override def toString = "macro(" + name + "=" +presenceCondition.resolveToExternal.toString +  ")"
     override def satName = expandedName
     def countSize() = 1
     def isExternal = false
+    override def collectDistinctFeatures=presenceCondition.resolveToExternal.collectDistinctFeatures
+    override def collectDistinctFeatureObjects=presenceCondition.resolveToExternal.collectDistinctFeatureObjects
 }
 
 object DefinedMacro {

commit 5d6b045b5edb391ced0a48f49dafcc49fec9c303

fixed a bug introduced by commit 74426be70bf8d339c7ff2491b5b20f3bd93ebed2

FeatureExprLib/src/main/scala/de/fosd/typechef/featureexpr/sat/SATFeatureExpr.scala
@@ -994,8 +994,9 @@ class DefinedMacro(val name: String, val presenceCondition: SATFeatureExpr, val 
     override def satName = expandedName
     def countSize() = 1
     def isExternal = false
-    override def collectDistinctFeatures=presenceCondition.resolveToExternal.collectDistinctFeatures
-    override def collectDistinctFeatureObjects=presenceCondition.resolveToExternal.collectDistinctFeatureObjects
+    /**TODO: This probably would be the correct way, but it breaks my product generation code, and i cannot fix it right now */
+    //override def collectDistinctFeatures=presenceCondition.resolveToExternal.collectDistinctFeatures
+    //override def collectDistinctFeatureObjects=presenceCondition.resolveToExternal.collectDistinctFeatureObjects
 }
 
 object DefinedMacro {