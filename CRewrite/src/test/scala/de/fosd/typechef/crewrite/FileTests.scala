5882fcc8d2558ed740285a31ce93fa980e6fc93c
Jörg Liebig <joliebig@fim.uni-passau.de>
2013-04-04 13:36:31 +0200

    remove code for product derivation from check cfg

--- CRewrite/src/test/scala/de/fosd/typechef/crewrite/FileTests.scala
+++ CRewrite/src/test/scala/de/fosd/typechef/crewrite/FileTests.scala
@@ -11,36 +11,6 @@ import de.fosd.typechef.conditional.{Opt, Choice}
 class FileTests extends TestHelper with EnforceTreeHelper with ConditionalControlFlow with ConditionalNavigation with CFGHelper {
   val folder = "testfiles/"
 
-  def deriveProductFromConfiguration[T <: Product](a: T, c: Configuration, env: ASTEnv): T = {
-    // manytd is crucial here; consider the following example
-    // Product1( c1, c2, c3, c4, c5)
-    // all changes the elements top down, so the parent is changed before the children and this
-    // way the lookup env.featureExpr(x) will not fail. Using topdown or everywherebu changes the children and so also the
-    // parent before the parent is processed so we get a NullPointerExceptions calling env.featureExpr(x). Reason is
-    // changed children lead to changed parent and a new hashcode so a call to env fails.
-    val pconfig = manytd(rule {
-      case Choice(feature, thenBranch, elseBranch) => if (c.config implies (if (env.containsASTElem(thenBranch)) env.featureExpr(thenBranch) else FeatureExprFactory.True) isTautology()) thenBranch else elseBranch
-      case l: List[Opt[_]] => {
-        var res: List[Opt[_]] = List()
-        // use l.reverse here to omit later reverse on res or use += or ++= in the thenBranch
-        for (o <- l.reverse)
-          if (o.feature == FeatureExprFactory.True)
-            res ::= o
-          else if (c.config implies (if (env.containsASTElem(o)) env.featureExpr(o) else FeatureExprFactory.True) isTautology()) {
-            res ::= o.copy(feature = FeatureExprFactory.True)
-          }
-        res
-      }
-      // we need ast here because otherwise we have old and new elements in the resulting product
-      // and this might pollute our caches later
-      case a: AST => a.clone()
-    })
-
-    val x = pconfig(a).get.asInstanceOf[T]
-    assert(isVariable(x) == false, "product still contains variability")
-    x
-  }
-
   private def checkCFG(filename: String, fm: FeatureModel = FeatureExprFactory.default.featureModelFactory.empty) = {
     var errorOccured = false
 
