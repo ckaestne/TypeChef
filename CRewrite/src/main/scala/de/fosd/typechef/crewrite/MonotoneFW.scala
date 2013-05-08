c8fb464c4ad8a1ec21ac8fe5c86919a7788c9a3e
Jörg Liebig <joliebig@fim.uni-passau.de>
2013-05-08 18:54:31 +0200

    update comments for monotonefw implementation

--- CRewrite/src/main/scala/de/fosd/typechef/crewrite/MonotoneFW.scala
+++ CRewrite/src/main/scala/de/fosd/typechef/crewrite/MonotoneFW.scala
@@ -6,12 +6,31 @@ import de.fosd.typechef.parser.c._
 import de.fosd.typechef.typesystem.UseDeclMap
 import de.fosd.typechef.featureexpr.{FeatureExprFactory, FeatureModel, FeatureExpr}
 
-// this trait provides standard routines of the monotone framework
-// (a general framework) for dataflow analyses such as liveness,
-// available expression, ...
-// in contrast to the original idea this implementation
-// is variability-aware; for more information about monotone frameworks
+// this abstract class provides a standard implementation of
+// the monotone framework, a general framework for dataflow analyses
+// such as liveness, available expression, double-free, ...
+// in contrast to the original, this implementation is variability-aware
+// and is able to handle variable shadowing
+//
+// the analysis itself is not precise, i.e., during the computation the feature
+// model is not used, especially for the predecessor and successor determintion.
+// the reason is, the predecessor/successor determination requires a lot of sat
+// calls. for larger case studies (e.g., linux) a single sat-call takes 1/2 second
+// and therefore predecessor/successor determination only takes the #ifdef annotations
+// in the files into consideration
+//
+// if a proper feature model is passed to the constructor of this class, we use
+// it to filter out false positives due to the imprecise predecessor and successor
+// determination
+//
+// for more information about monotone frameworks
 // see "Principles of Program Analysis" by (Nielson, Nielson, Hankin)
+
+// env: ASTEnv; environment used for navigation in the AST during predecessor and
+//              successor determination
+// udm: UseDeclMap; map of variable usages to their corresponding declarations
+//                  a variable here can have different declarations (alternative types)
+// fm: FeatureModel; feature model used for filtering out false positives
 abstract class MonotoneFW[T](val env: ASTEnv, val udm: UseDeclMap, val fm: FeatureModel) extends AttributionBase with IntraCFG {
 
     // since C allows variable shadowing we need to track variable usages
@@ -23,7 +42,6 @@ abstract class MonotoneFW[T](val env: ASTEnv, val udm: UseDeclMap, val fm: Featu
     protected val exit_cache = new IdentityHashMapCache[Map[T, FeatureExpr]]()
 
     // add annotation to elements of a Set[Id]
-    // used for uses, defines, and declares
     protected def addAnnotation2ResultSet(in: Set[Id]): Map[FeatureExpr, Set[Id]] = {
         var res = Map[FeatureExpr, Set[Id]]()
 
@@ -40,6 +58,7 @@ abstract class MonotoneFW[T](val env: ASTEnv, val udm: UseDeclMap, val fm: Featu
         res
     }
 
+    // gen and kill function, will be implemented by the concrete dataflow classes
     def gen(a: AST): Map[FeatureExpr, Set[T]]
     def kill(a: AST): Map[FeatureExpr, Set[T]]
 
@@ -91,6 +110,9 @@ abstract class MonotoneFW[T](val env: ASTEnv, val udm: UseDeclMap, val fm: Featu
         }
     }
 
+    // flow functions (flow => succ and flow_R) functions of the
+    // framework directly encoded in analysis_exit_backward
+    // and analysis_exit_forward
     protected val analysis_exit: AST => Map[T, FeatureExpr]
 
     protected val analysis_exit_backward: AST => Map[T, FeatureExpr] =
@@ -124,7 +146,9 @@ abstract class MonotoneFW[T](val env: ASTEnv, val udm: UseDeclMap, val fm: Featu
             }
         }
 
-    def exit(a: AST) = {
+    // using caching for efficiency and filtering out false positives
+    // using the feature model
+    private def exit(a: AST) = {
         exit_cache.lookup(a) match {
             case Some(v) => v
             case None => {
@@ -137,7 +161,7 @@ abstract class MonotoneFW[T](val env: ASTEnv, val udm: UseDeclMap, val fm: Featu
 
     def out(a: AST) = exit(a).filter(_._2.isSatisfiable(fm))
 
-    def entry(a: AST) = {
+    private def entry(a: AST) = {
         entry_cache.lookup(a) match {
             case Some(v) => v
             case None => {
