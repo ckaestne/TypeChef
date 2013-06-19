3f3bb42937a065258245ab90d7c8efbc117da9ed
aJanker <janker@fim.uni-passau.de>
2013-06-19 15:37:59 +0200

    Improved Random Id Selection

--- CRefactor/src/test/scala/de/fosd/typechef/crefactor/BusyBoxEvaluation/RenameEvaluation.scala
+++ CRefactor/src/test/scala/de/fosd/typechef/crefactor/BusyBoxEvaluation/RenameEvaluation.scala
@@ -40,7 +40,8 @@ class RenameEvaluation extends BusyBoxEvaluation {
             val amountOfIds = RenameIdentifier.getAllConnectedIdentifier(id, morpheus.getDeclUseMap, morpheus.getUseDeclMap).length
             val features = RenameIdentifier.getAllConnectedIdentifier(id, morpheus.getDeclUseMap, morpheus.getUseDeclMap).map(x => morpheus.getASTEnv.featureExpr(x))
             // check recursive only for variable ids
-            if (!(new File(id.getPositionFrom.getFile).canWrite) && id.name.equals("main")) getVariableIdForRename(depth + 1)
+            val writeAble = RenameIdentifier.getAllConnectedIdentifier(id, morpheus.getDeclUseMap, morpheus.getUseDeclMap).forall(i => new File(i.getPositionFrom.getFile).canWrite)
+            if (!writeAble || id.name.equals("main")) getVariableIdForRename(depth + 1)
             else if ((features.distinct.length == 1) && features.contains("True") && FORCE_VARIABILITY && (depth < MAX_DEPTH)) getVariableIdForRename(depth + 1)
             else (id, amountOfIds, features)
         }
