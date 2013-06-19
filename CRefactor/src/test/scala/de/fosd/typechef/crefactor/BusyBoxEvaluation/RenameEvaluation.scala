243677875b63451cda0ad2993877b2adb97523fc
ajanker <an.janker@googlemail.com>
2013-06-19 13:40:19 +0200

    changed typechef options

--- CRefactor/src/test/scala/de/fosd/typechef/crefactor/BusyBoxEvaluation/RenameEvaluation.scala
+++ CRefactor/src/test/scala/de/fosd/typechef/crefactor/BusyBoxEvaluation/RenameEvaluation.scala
@@ -2,20 +2,20 @@ package de.fosd.typechef.crefactor.BusyBoxEvaluation
 
 import org.junit.Test
 import java.io.File
-import de.fosd.typechef.parser.c.AST
+import de.fosd.typechef.parser.c.{PrettyPrinter, AST}
 import de.fosd.typechef.featureexpr.FeatureExpr
 
 class RenameEvaluation extends BusyBoxEvaluation {
     @Test
     def evaluate() {
         val files = getBusyBoxFiles
-        files.forall(file => performRefactor(new File(busyBoxPath + file)))
+        files.map(file => performRefactor(new File(busyBoxPath + file)))
     }
     def performRefactor(fileToRefactor: File): Boolean = {
         val parsed = parse(fileToRefactor)
         val ast = parsed._1
         val fm = parsed._2
-        println(ast)
+        println(PrettyPrinter.print(ast))
         false
     }
 
