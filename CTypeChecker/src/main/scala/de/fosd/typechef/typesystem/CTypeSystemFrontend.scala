0cc2c1f5b32a5d69b042655a2fd5ea1b259a6eab
Jörg Liebig <joliebig@fim.uni-passau.de>
2013-06-20 16:10:04 +0200

    fix intellij warnings

--- CTypeChecker/src/main/scala/de/fosd/typechef/typesystem/CTypeSystemFrontend.scala
+++ CTypeChecker/src/main/scala/de/fosd/typechef/typesystem/CTypeSystemFrontend.scala
@@ -25,29 +25,27 @@ class CTypeSystemFrontend(iast: TranslationUnit,
     def prettyPrintType(ctype: Conditional[CType]): String =
         Conditional.toOptList(ctype).map(o => o.feature.toString + ": \t" + o.entry).mkString("\n")
 
-    private def indentAllLines(s: String): String =
-        s.lines.map("\t\t" + _).foldLeft("")(_ + "\n" + _)
-
     var errors: List[TypeChefError] = List()
 
     var isSilent = false
 
     val DEBUG_PRINT = false
 
-    def dbgPrint(o: Any) = if (DEBUG_PRINT) print(o)
+    def dbgPrint(o: Any) { if (DEBUG_PRINT) print(o) }
 
-    def dbgPrintln(o: Any) = if (DEBUG_PRINT) println(o)
+    def dbgPrintln(o: Any) { if (DEBUG_PRINT) println(o) }
 
     val verbose = false
 
 
     var externalDefCounter: Int = 0
-    override def checkingExternal(externalDef: ExternalDef) = {
+    override def checkingExternal(externalDef: ExternalDef) {
         externalDefCounter = externalDefCounter + 1
         if (verbose)
             println("check " + externalDefCounter + "/" + iast.defs.size + ". line " + externalDef.getPositionFrom.getLine + ". err " + errors.size)
     }
-    override def issueTypeError(severity: Severity.Severity, condition: FeatureExpr, msg: String, where: AST, severityExtra: String = "") =
+
+    override def issueTypeError(severity: Severity.Severity, condition: FeatureExpr, msg: String, where: AST, severityExtra: String = "") {
     	//first check without feature model for performance reasons
         if (condition.isSatisfiable() && condition.isSatisfiable(featureModel)) {
             val e = new TypeChefError(severity, condition, msg, where, severityExtra)
@@ -55,6 +53,7 @@ class CTypeSystemFrontend(iast: TranslationUnit,
             if (!isSilent) {
             	println("  - " + e)
         	}
+        }
     }
 
 
@@ -72,15 +71,15 @@ class CTypeSystemFrontend(iast: TranslationUnit,
         if (merrors.isEmpty)
             println("No type errors found.")
         else {
-            println("Found " + merrors.size + " type errors: ");
+            println("Found " + merrors.size + " type errors: ")
         }
         //println("\n")
-        return merrors.isEmpty
+        merrors.isEmpty
     }
     def checkASTSilent: Boolean = {
         isSilent = true
         errors = List() // clear error list
         typecheckTranslationUnit(iast)
-        return errors.isEmpty
+        errors.isEmpty
     }
 }
