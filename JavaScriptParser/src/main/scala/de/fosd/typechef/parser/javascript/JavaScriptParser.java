f00db3318843d8d75f0dde4354b0bac036995552
Christian Kaestner <kaestner@cs.cmu.edu>
2013-10-24 14:08:20 -0400

    replaced file?

---
+++ JavaScriptParser/src/main/scala/de/fosd/typechef/parser/javascript/JavaScriptParser.java
@@ -0,0 +1,11 @@
+package de.fosd.typechef.parser.javascript;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ckaestne
+ * Date: 10/17/13
+ * Time: 1:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JavaScriptParser {
+}
