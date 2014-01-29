c35a3bb6a3ea77b462a3783506b68ce78db15432
Christian Kaestner <kaestner@cs.cmu.edu>
2014-01-29 17:48:54 -0500

    normalize whitespace

--- CRewrite/src/test/scala/de/fosd/typechef/crewrite/InterCFGTest.scala
+++ CRewrite/src/test/scala/de/fosd/typechef/crewrite/InterCFGTest.scala
@@ -10,35 +10,35 @@ import de.fosd.typechef.crewrite.asthelper.CASTEnv
 
 class InterCFGTest extends InterCFG with TestHelper with CFGHelper {
 
-  @Test def test_two_functions() {
-    val folder = "testfiles/"
-    val filename = "intercfgtest01.c"
-    val is: InputStream = getClass.getResourceAsStream("/" + folder + filename)
-    if (is == null)
-      throw new FileNotFoundException("Input file not fould!")
+    @Test def test_two_functions() {
+        val folder = "testfiles/"
+        val filename = "intercfgtest01.c"
+        val is: InputStream = getClass.getResourceAsStream("/" + folder + filename)
+        if (is == null)
+            throw new FileNotFoundException("Input file not fould!")
 
-    val ast = parseFile(is, folder, filename)
-    val env = CASTEnv.createASTEnv(ast)
+        val ast = parseFile(is, folder, filename)
+        val env = CASTEnv.createASTEnv(ast)
 
-    val fdefs = filterAllASTElems[FunctionDef](ast)
+        val fdefs = filterAllASTElems[FunctionDef](ast)
 
-    val a = new StringWriter()
-    val dot = new DotGraph(a)
-    dot.writeHeader("test")
+        val a = new StringWriter()
+        val dot = new DotGraph(a)
+        dot.writeHeader("test")
 
-    val fsuccs = fdefs.map(getAllSucc(_, env))
+        val fsuccs = fdefs.map(getAllSucc(_, env))
 
-      def lookupFExpr(e: AST): FeatureExpr = e match {
-          case o if env.isKnown(o) => env.featureExpr(o)
-          case _ => FeatureExprFactory.True
-      }
+        def lookupFExpr(e: AST): FeatureExpr = e match {
+            case o if env.isKnown(o) => env.featureExpr(o)
+            case _ => FeatureExprFactory.True
+        }
 
-    for (s <- fsuccs)
-      dot.writeMethodGraph(s, lookupFExpr, "")
-    dot.writeFooter()
-    dot.close()
-    println(a)
-  }
+        for (s <- fsuccs)
+            dot.writeMethodGraph(s, lookupFExpr, "")
+        dot.writeFooter()
+        dot.close()
+        println(a)
+    }
     // provide a lookup mechanism for function defs (from the type system or selfimplemented)
     def getTranslationUnit(): TranslationUnit = null
 }
