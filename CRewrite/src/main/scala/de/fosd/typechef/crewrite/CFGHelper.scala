cb38b9bd822b275a298034a761580eca06625394
Jörg Liebig <joliebig@fim.uni-passau.de>
2014-03-10 14:14:50 +0100

    renaming refactoring

--- CRewrite/src/main/scala/de/fosd/typechef/crewrite/CFGHelper.scala
+++ CRewrite/src/main/scala/de/fosd/typechef/crewrite/CFGHelper.scala
@@ -30,21 +30,20 @@ trait CFGHelper extends IntraCFG {
 
     // determine recursively all pred
     def getAllPred(i: AST, env: ASTEnv): SuccessorRelationship = {
-        var r = List[(AST, NextNodeList)]()
-        var s = List(i)
-        var d = List[AST]()
-        var c: AST = null
+        var result = List[(AST, NextNodeList)]()
+        var workingList = List(i)
+        var finishedNodes = List[AST]()
 
-        while (!s.isEmpty) {
-            c = s.head
-            s = s.drop(1)
+        while (!workingList.isEmpty) {
+            val curNode = workingList.head
+            workingList = workingList.tail
 
-            if (d.filter(_.eq(c)).isEmpty) {
-                r = (c, pred(c, env)) :: r
-                s = s ++ r.head._2.map(x => x.entry)
-                d = d ++ List(c)
+            if (finishedNodes.filter(_.eq(curNode)).isEmpty) {
+                result = (curNode, pred(curNode, env)) :: result
+                workingList = workingList ++ result.head._2.map(x => x.entry)
+                finishedNodes = finishedNodes ++ List(c)
             }
         }
-        r
+        result
     }
 }
