25a3090346a48203000eeb6f6ed6f04a02e10368
Christian Kaestner <kaestner@cs.cmu.edu>
2014-03-26 16:22:59 -0400

    replace deprecated method call from updated testing library
    replace expect() with expectResult()

--- ParserFramework/src/test/scala/de/fosd/typechef/parser/test/BenchmarkTest.scala
+++ ParserFramework/src/test/scala/de/fosd/typechef/parser/test/BenchmarkTest.scala
@@ -19,26 +19,26 @@ class BenchmarkTest extends TestCase with DigitListUtilities {
 
     val p = new CharDigitParser()
 
-    @Test def testTokenCounter1() = expect(List(t("1"), t("2")), 2, 0, 0, p.twosymb)
-    @Test def testTokenCounter2() = expect(List(t("a"), t("2")), 2, 0, 0, p.twosymb)
+    @Test def testTokenCounter1() = expectResult(List(t("1"), t("2")), 2, 0, 0, p.twosymb)
+    @Test def testTokenCounter2() = expectResult(List(t("a"), t("2")), 2, 0, 0, p.twosymb)
     //requires to backtrack once
-    @Test def testTokenCounter3() = expect(List(t("a"), t("2")), 3, 1, 0, p.ab)
+    @Test def testTokenCounter3() = expectResult(List(t("a"), t("2")), 3, 1, 0, p.ab)
 
     //split parsing, but consume every token only once
-    @Test def testSplitParsing1() = expect(List(t("1", f1), t("2", f1.not)), 2, 0, 0, p.digit)
+    @Test def testSplitParsing1() = expectResult(List(t("1", f1), t("2", f1.not)), 2, 0, 0, p.digit)
     //split parsing, and consume last token twice
-    @Test def testSplitParsing2() = expect(List(t("("), t("1", f1), t("2", f1.not), t(")")), 5, 0, 1, p.parenDigit)
+    @Test def testSplitParsing2() = expectResult(List(t("("), t("1", f1), t("2", f1.not), t(")")), 5, 0, 1, p.parenDigit)
     //split parsing, but backtracks only in one branch -- which is considered as replicate parsing
-    @Test def testSplitParsing3() = expect(List(t("a"), t("b", f1), t("1", f1.not)), 4, 0, 1, p.ab)
+    @Test def testSplitParsing3() = expectResult(List(t("a"), t("b", f1), t("1", f1.not)), 4, 0, 1, p.ab)
     //split parsing, backtracking as in previous, and replicate parsing at closing bracket
-    @Test def testSplitParsing4() = expect(List(t("("), t("a"), t("b", f1), t("1", f1.not), t(")")), 7, 0, 2, p.parenAb)
+    @Test def testSplitParsing4() = expectResult(List(t("("), t("a"), t("b", f1), t("1", f1.not), t(")")), 7, 0, 2, p.parenAb)
 
     //1 * 2 + 3_ 4_
-    @Test def testExpr1 = expect(List(t("1"), t("*"), t("2", f1), t("3", f1.not), t("+"), t("3")), 6, 0, 0, p.expr)
+    @Test def testExpr1 = expectResult(List(t("1"), t("*"), t("2", f1), t("3", f1.not), t("+"), t("3")), 6, 0, 0, p.expr)
     //1 * (_ 2 + 3 )_
-    @Test def testExpr2 = expect(List(t("1"), t("*"), t("(", f1), t("2"), t("+"), t("3"), t(")", f1)), 10, 0, 3, p.expr)
+    @Test def testExpr2 = expectResult(List(t("1"), t("*"), t("(", f1), t("2"), t("+"), t("3"), t(")", f1)), 10, 0, 3, p.expr)
 
-    private def expect(l: List[MyToken], tokenCounter: Int, backtracking: Int, replicatedParsing: Int, parser: p.MultiParser[Any]) {
+    private def expectResult(l: List[MyToken], tokenCounter: Int, backtracking: Int, replicatedParsing: Int, parser: p.MultiParser[Any]) {
         val in = p.tr(l)
         println("----")
         println("tokens: " + l + " -- " + in.tokens.size)
