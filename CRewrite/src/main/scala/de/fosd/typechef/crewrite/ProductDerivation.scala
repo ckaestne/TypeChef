ad7c8d738049d9d14899c4106585067dc80f7858
Jörg Liebig <joliebig@fim.uni-passau.de>
2014-04-08 19:09:58 +0200

    simplify product derivation

--- CRewrite/src/main/scala/de/fosd/typechef/crewrite/ProductDerivation.scala
+++ CRewrite/src/main/scala/de/fosd/typechef/crewrite/ProductDerivation.scala
@@ -2,7 +2,6 @@ package de.fosd.typechef.crewrite
 
 import de.fosd.typechef.featureexpr.FeatureExprFactory
 import de.fosd.typechef.conditional.{Choice, Opt}
-import de.fosd.typechef.parser.c.{AST}
 import org.kiama.rewriting.Rewriter._
 import de.fosd.typechef.crewrite.asthelper.EnforceTreeHelper
 
@@ -11,20 +10,12 @@ object ProductDerivation {
         assert(ast != null)
 
         val prod = manytd(rule {
-            case l: List[Opt[_]] => {
-                var res: List[Opt[_]] = List()
-                // use l.reverse here to omit later reverse on res or use += or ++= in the thenBranch
-                for (o <- l.reverse)
-                    if (o.feature.evaluate(selectedFeatures)) {
-                        res ::= o.copy(feature = FeatureExprFactory.True)
-                    }
-                res
-            }
-            case Choice(feature, thenBranch, elseBranch) => {
+            case l: List[Opt[_]] =>
+                l.filter { o => o.feature.evaluate(selectedFeatures) }
+                .map { o => o.copy(feature = FeatureExprFactory.True) }
+            case Choice(feature, thenBranch, elseBranch) =>
                 if (feature.evaluate(selectedFeatures)) thenBranch
                 else elseBranch
-            }
-            case a: AST => a.clone()
         })
         val cast = prod(ast).get.asInstanceOf[T]
         EnforceTreeHelper.copyPositions(ast, cast)
