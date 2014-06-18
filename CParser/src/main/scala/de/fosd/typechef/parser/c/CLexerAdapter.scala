65cd427f1b47cb06da6116ade76aa63e6e86756f
Jörg Liebig <joliebig@fim.uni-passau.de>
2014-06-18 19:16:52 +0200

    minor lexer/parser changes

--- CParser/src/main/scala/de/fosd/typechef/parser/c/CLexerAdapter.scala
+++ CParser/src/main/scala/de/fosd/typechef/parser/c/CLexerAdapter.scala
@@ -8,6 +8,8 @@ import de.fosd.typechef.LexerToken
 import de.fosd.typechef.conditional.Conditional
 import de.fosd.typechef.featureexpr.FeatureExprFactory.True
 import de.fosd.typechef.lexer.LexerFrontend.LexerSuccess
+import java.io.InputStream
+import de.fosd.typechef.featureexpr.{FeatureExprFactory, FeatureModel}
 
 /**
  * wrapper for the partial preprocessor, which adapts the output of the lexer into the input of the parser
@@ -48,6 +50,9 @@ object CLexerAdapter {
         }).flatten((f,a,b)=>a ++ b)
         prepareTokens(tokens.toIterable)
     }
+
+    def lexStream(stream: InputStream, filePath: String, systemIncludePath: java.util.List[String], featureModel: FeatureModel = FeatureExprFactory.empty): TokenReader[TokenWrapper, CTypeContext] =
+        CLexerAdapter.prepareTokens(new LexerFrontend().parseStream(stream, filePath, systemIncludePath, featureModel))
 
 
 }
