feb0b5525d4e5a754ec0d5ba6648c8cd80bfde01
Christian Kaestner <kaestner@cs.cmu.edu>
2015-01-25 15:30:49 -0500

    additional struct test case to check assumptions

--- CTypeChecker/src/test/scala/de/fosd/typechef/typesystem/StructTest.scala
+++ CTypeChecker/src/test/scala/de/fosd/typechef/typesystem/StructTest.scala
@@ -601,4 +601,18 @@ struct reiserfs_sb_info {
 
     }
 
+    test("incomplete structs and scopes") {
+        assertResult(true) {
+            check( """
+                     |struct structname { int fieldname; };
+                     |
+                     |int main() {
+                     |        struct structname varname;
+                     |        varname.fieldname = 2;
+                     |        return b.fieldname;
+                     |}
+                   """.stripMargin)
+        }
+    }
+
 }
