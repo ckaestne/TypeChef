f395b0b7876bb44affe6526fb98432b0bbad1c91
Jörg Liebig <joerg.liebig@methodpark.de>
2015-09-10 19:12:36 +0200

    add regression test for dead-store

--- CRewrite/src/test/scala/de/fosd/typechef/crewrite/DeadStoreTest.scala
+++ CRewrite/src/test/scala/de/fosd/typechef/crewrite/DeadStoreTest.scala
@@ -74,4 +74,43 @@ void test1(int *code,
             }
             """.stripMargin) should be(false)
     }
+
+    @Test def test_sqlite_while() {
+        deadstore(
+        """
+          |static int sqlite3FtsUnicodeIsalnum(int c){
+          |  const static unsigned int aEntry[] = {
+          |    0x07D9140B, 0x07DA0046, 0x07DC0074, 0x38000401, 0x38008060,
+          |    0x380400F0,
+          |  };
+          |  static const unsigned int aAscii[4] = {
+          |    0xFFFFFFFF, 0xFC00FFFF, 0xF8000001, 0xF8000001,
+          |  };
+          |
+          |  if( c<128 ){
+          |    return ( (aAscii[c >> 5] & (1 << (c & 0x001F)))==0 );
+          |  }else if( c<(1<<22) ){
+          |    unsigned int key = (((unsigned int)c)<<10) | 0x000003FF;
+          |    int iRes;
+          |    int iHi = sizeof(aEntry)/sizeof(aEntry[0]) - 1;
+          |    int iLo = 0;
+          |    while( iHi>=iLo ){
+          |      int iTest = (iHi + iLo) / 2;
+          |      if( key >= aEntry[iTest] ){
+          |        iRes = iTest;
+          |        iLo = iTest+1;
+          |      }else{
+          |        iHi = iTest-1;
+          |      }
+          |    }
+          |    ((void) (0));
+          |    ((void) (0));
+          |    return (((unsigned int)c) >= ((aEntry[iRes]>>10) + (aEntry[iRes]&0x3FF)));
+          |  }
+          |  return 1;
+          |}
+          |
+        """.stripMargin
+        ) should be(true)
+    }
 }
