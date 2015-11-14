d765dcf8b8010d8abcf56e789cfe77a8791aacab
Joerg Liebig <joliebig@fim.uni-passau.de>
2015-11-14 23:09:25 +0100

    starting to rewrite control-flow computation

---
+++ CRewrite/src/test/scala/de/fosd/typechef/crewrite/CCFGTest.scala
@@ -0,0 +1,38 @@
+package de.fosd.typechef.crewrite
+
+import de.fosd.typechef.featureexpr.FeatureExprFactory
+import de.fosd.typechef.parser.c._
+import de.fosd.typechef.typesystem.{CDeclUse, CTypeCache, CTypeSystemFrontend}
+import org.junit.Test
+import org.scalatest.Matchers
+
+class CCFGTest extends TestHelper with Matchers with CCFG with EnforceTreeHelper {
+
+    def cfgtest(code: String): Boolean = {
+        val f = prepareAST[FunctionDef](parseFunctionDef(code))
+        val env = CASTEnv.createASTEnv(f)
+        getAllSucc(f, env).foreach {
+            case (e, s) =>
+                println(e + "====>")
+                s.foreach { println(_) }
+                println("############")
+        }
+        true
+    }
+
+    @Test def test_fdef() {
+        cfgtest( """
+              void foo() {
+              #ifdef A
+                int i;
+                int k;
+              #endif
+                while (j) {
+                  k;  // dead store
+                }
+                l;
+              }
+                    """.stripMargin) should be(true)
+    }
+
+}
