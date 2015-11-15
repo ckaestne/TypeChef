3de1af6ed46e0b7ad2348174c41a500a4b9f022b
Joerg Liebig <joliebig@fim.uni-passau.de>
2015-11-15 17:42:19 +0100

    move getAllSucc to testing code

--- CRewrite/src/test/scala/de/fosd/typechef/crewrite/CCFGTest.scala
+++ CRewrite/src/test/scala/de/fosd/typechef/crewrite/CCFGTest.scala
@@ -8,6 +8,26 @@ import org.scalatest.Matchers
 
 class CCFGTest extends TestHelper with Matchers with CCFG with EnforceTreeHelper {
 
+    // determine recursively all succs check
+    def getAllSucc(i: AST, env: ASTEnv) = {
+        var r = List[(AST, CFG)]()
+        var s = List(i)
+        var d = List[AST]()
+        var c: AST = null
+
+        while (s.nonEmpty) {
+            c = s.head
+            s = s.drop(1)
+
+            if (!d.exists(_.eq(c))) {
+                r = (c, succ(env)(c)) :: r
+                s = s ++ r.head._2.map(x => x.entry)
+                d = d ++ List(c)
+            }
+        }
+        r
+    }
+
     def cfgtest(code: String): Boolean = {
         val f = prepareAST[FunctionDef](parseFunctionDef(code))
         val env = CASTEnv.createASTEnv(f)
@@ -28,11 +48,12 @@ class CCFGTest extends TestHelper with Matchers with CCFG with EnforceTreeHelper
     @Test def test_fdef() {
         cfgtest( """
               void foo() {
-              #ifdef A
-                a;
+              while (a) {
+                #ifdef B
                 b;
-                c;
-              #endif
+                #endif
+              }
+              c;
               }
                     """.stripMargin) should be(true)
     }
